c1217b4f041001ae735dbb332046783917eb8822
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-12-25 14:46:17 +0300

    Проверки для типа double

---
+++ src/main/java/ru/d_shap/assertions/primitive/DoubleAssertion.java
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for unit testing.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of Assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.primitive;
+
+import ru.d_shap.assertions.BaseAssertion;
+
+/**
+ * Assertions for the double primitive value.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class DoubleAssertion extends BaseAssertion {
+
+    private final double _actual;
+
+    /**
+     * Create new object.
+     *
+     * @param actual  the actual value.
+     * @param message the error message.
+     */
+    public DoubleAssertion(final double actual, final String message) {
+        super(message);
+        _actual = actual;
+    }
+
+    /**
+     * Check if the actual value is equal to the expected value.
+     *
+     * @param expected the expected value.
+     * @param delta    maximum delta between the actual value and the expected value.
+     */
+    public final void isEqualTo(final double expected, final double delta) {
+        if (Double.compare(expected, delta) != 0 && Math.abs(expected - delta) > delta) {
+            throw new AssertionError();
+        }
+    }
+
+    /**
+     * Check if the actual value is NOT equal to the expected value.
+     *
+     * @param expected the expected value.
+     * @param delta    maximum delta between the actual value and the expected value.
+     */
+    public final void isNotEqualTo(final double expected, final double delta) {
+        if (Double.compare(expected, delta) == 0 || Math.abs(expected - delta) <= delta) {
+            throw new AssertionError();
+        }
+    }
+
+    /**
+     * Check if the actual value is greater than the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void isGreaterThan(final double expected) {
+        if (_actual <= expected) {
+            fail("Value should be greater then the expected. Expected: <" + expected + ">, but was: <" + _actual + ">");
+        }
+    }
+
+    /**
+     * Check if the actual value is greater than or equal to the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void isGreaterThanOrEqualTo(final double expected) {
+        if (_actual < expected) {
+            fail("Value should be greater then or equal to the expected. Expected: <" + expected + ">, but was: <" + _actual + ">");
+        }
+    }
+
+    /**
+     * Check if the actual value is less than the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void isLessThan(final double expected) {
+        if (_actual >= expected) {
+            fail("Value should be less then the expected. Expected: <" + expected + ">, but was: <" + _actual + ">");
+        }
+    }
+
+    /**
+     * Check if the actual value is less than or equal to the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void isLessThanOrEqualTo(final double expected) {
+        if (_actual > expected) {
+            fail("Value should be less then or equal to the expected. Expected: <" + expected + ">, but was: <" + _actual + ">");
+        }
+    }
+
+}
