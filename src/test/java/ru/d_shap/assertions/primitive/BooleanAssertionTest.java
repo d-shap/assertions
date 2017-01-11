9550a72c67b94ad4aa6900bb42694aaaaa5b1208
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-01-11 12:44:47 +0300

    Тесты для логических предположений

---
+++ src/test/java/ru/d_shap/assertions/primitive/BooleanAssertionTest.java
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for unit testing.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of Assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.primitive;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link BooleanAssertion}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class BooleanAssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public BooleanAssertionTest() {
+        super();
+    }
+
+    /**
+     * {@link BooleanAssertion} class test.
+     */
+    @Test
+    public void isTrueTest() {
+        new BooleanAssertion(true, null).isTrue();
+        try {
+            new BooleanAssertion(false, null).isTrue();
+            Assertions.fail("Boolean assertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Value should be true.");
+        }
+    }
+
+    /**
+     * {@link BooleanAssertion} class test.
+     */
+    @Test
+    public void isFalseTest() {
+        new BooleanAssertion(false, null).isFalse();
+        try {
+            new BooleanAssertion(true, null).isFalse();
+            Assertions.fail("Boolean assertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Value should be false.");
+        }
+    }
+
+}
