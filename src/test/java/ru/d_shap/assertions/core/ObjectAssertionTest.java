274c616f115ab8a0a73b8e74e594393b1ae00c06
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-01-14 22:42:29 +0300

    Добавил тестов для утверждений object

---
+++ src/test/java/ru/d_shap/assertions/core/ObjectAssertionTest.java
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for unit testing.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of Assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.core;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+
+/**
+ * Tests for {@link ObjectAssertion}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ObjectAssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public ObjectAssertionTest() {
+        super();
+    }
+
+    /**
+     * {@link ObjectAssertion} class test.
+     */
+    @Test
+    public void asStringTest() {
+        Assertions.assertThat(new ObjectAssertion(new Object(), null).asString("value")).isEqualTo("value");
+        Assertions.assertThat(new ObjectAssertion(new Object(), null).asString(1)).isEqualTo("1");
+        Assertions.assertThat(new ObjectAssertion(new Object(), null).asString(new StringBuilder("builder"))).isEqualTo("builder");
+    }
+
+}
