fae2f53b5b0718b0208ff67ff7ada63827bcd4fd
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-01-24 07:37:44 +0300

    Добавил утверждений

--- src/main/java/ru/d_shap/assertions/collection/IteratorAssertion.java
+++ src/main/java/ru/d_shap/assertions/collection/IteratorAssertion.java
@@ -20,8 +20,11 @@
 package ru.d_shap.assertions.collection;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import ru.d_shap.assertions.ReferenceAssertion;
+import ru.d_shap.assertions.primitive.IntAssertion;
 
 /**
  * Assertions for the iterator.
@@ -33,20 +36,139 @@ public class IteratorAssertion extends ReferenceAssertion {
     /**
      * Create new object.
      *
-     * @param actual  the actual iterator.
+     * @param actual  the actual value.
      * @param message the assertion message.
      */
     public IteratorAssertion(final Iterator<?> actual, final String message) {
         super(actual, message);
     }
 
+    /**
+     * Check if the actual value is empty.
+     */
+    public final void isEmpty() {
+        createCollectionAssertion().isEmpty();
+    }
+
+    /**
+     * Check if the actual value is null or empty.
+     */
+    public final void isNullOrEmpty() {
+        if (getActual() != null) {
+            createCollectionAssertion().isEmpty();
+        }
+    }
+
+    /**
+     * Check if the actual value is NOT empty.
+     */
+    public final void isNotEmpty() {
+        createCollectionAssertion().isNotEmpty();
+    }
+
+    /**
+     * Check if the actual value contains the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void contains(final Object expected) {
+        createCollectionAssertion().contains(expected);
+    }
+
+    /**
+     * Check if the actual value does NOT contain the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void doesNotContain(final Object expected) {
+        createCollectionAssertion().doesNotContain(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAll(final Object... expected) {
+        createCollectionAssertion().containsAll(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values in any order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAllInAnyOrder(final Object... expected) {
+        createCollectionAssertion().containsAllInAnyOrder(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactly(final Object... expected) {
+        createCollectionAssertion().containsExactly(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly in any order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactlyInAnyOrder(final Object... expected) {
+        createCollectionAssertion().containsExactlyInAnyOrder(expected);
+    }
+
+    /**
+     * Check if the actual value contains any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAny(final Object... expected) {
+        createCollectionAssertion().containsAny(expected);
+    }
+
+    /**
+     * Check if the actual value does NOT contain any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsNone(final Object... expected) {
+        createCollectionAssertion().containsNone(expected);
+    }
+
+    /**
+     * Make assertion about the actual collection size.
+     *
+     * @return assertion.
+     */
+    public final IntAssertion toSize() {
+        return createCollectionAssertion().toSize();
+    }
+
+    /**
+     * Check if the actual value size is equal to the expected collection size.
+     *
+     * @param expected the expected collection size.
+     */
+    public final void hasSize(final int expected) {
+        createCollectionAssertion().hasSize(expected);
+    }
+
+    private CollectionAssertion createCollectionAssertion() {
+        Iterator<?> iterator = (Iterator<?>) getActual();
+        List<Object> result = new LinkedList<>();
+        while (iterator.hasNext()) {
+            Object item = iterator.next();
+            result.add(item);
+        }
+        return new ListAssertion(result, getMessage());
+    }
+
     @Override
     protected final String asString(final Object value) {
-        if (value == null) {
-            return null;
-        } else {
-            return String.valueOf(value);
-        }
+        return null;
     }
 
 }
