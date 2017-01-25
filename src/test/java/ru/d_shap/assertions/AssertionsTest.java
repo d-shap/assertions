cce3aa384b1c8348cc7856bd24d2a4d2981766fb
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-01-25 07:53:37 +0300

    Тесты для assertions

---
+++ src/test/java/ru/d_shap/assertions/AssertionsTest.java
@@ -0,0 +1,527 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for unit testing.
+// Copyright (C) 2016 Dmitry Shapovalov.
+//
+// This file is part of Assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link Assertions}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class AssertionsTest {
+
+    /**
+     * Test class constructor.
+     */
+    public AssertionsTest() {
+        super();
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void constructorTest() {
+        Assertions.assertThat(Assertions.class).hasOnePrivateConstructor();
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void assertWithMessageTest() {
+        Assertions.assertWithMessage("Simple test").that(5).isEqualTo(5);
+
+        try {
+            Assertions.assertWithMessage("Simple test").that(5).isEqualTo(6);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Simple test. Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void byteAssertionTest() {
+        Assertions.assertThat((byte) 5).isEqualTo(5);
+
+        try {
+            Assertions.assertThat((byte) 5).isEqualTo(6);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void shortAssertionTest() {
+        Assertions.assertThat((short) 5).isEqualTo(5);
+
+        try {
+            Assertions.assertThat((short) 5).isEqualTo(6);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void intAssertionTest() {
+        Assertions.assertThat(5).isEqualTo(5);
+
+        try {
+            Assertions.assertThat(5).isEqualTo(6);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void longAssertionTest() {
+        Assertions.assertThat(5L).isEqualTo(5L);
+
+        try {
+            Assertions.assertThat(5L).isEqualTo(6L);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void floatAssertionTest() {
+        Assertions.assertThat(5.0f).isEqualTo(5.0f, 0.001f);
+
+        try {
+            Assertions.assertThat(5.0f).isEqualTo(6.0f, 0.001f);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6.0> but was:<5.0>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void doubleAssertionTest() {
+        Assertions.assertThat(5.0).isEqualTo(5.0, 0.001);
+
+        try {
+            Assertions.assertThat(5.0).isEqualTo(6.0, 0.001);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6.0> but was:<5.0>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void booleanAssertionTest() {
+        Assertions.assertThat(true).isTrue();
+
+        try {
+            Assertions.assertThat(true).isFalse();
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Value should be false.");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void charAssertionTest() {
+        Assertions.assertThat('5').isEqualTo('5');
+
+        try {
+            Assertions.assertThat('5').isEqualTo('6');
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6(54)> but was:<5(53)>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void objectAssertionTest() {
+        Object object = new StringBuilder("value");
+        Assertions.assertThat(object).isSameAs(object);
+
+        try {
+            Assertions.assertThat(object).isSameAs(new StringBuilder("another value"));
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<another value> but was:<value>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void byteArrayAssertionTest() {
+        Assertions.assertThat(new byte[]{1, 2, 3}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new byte[]{1, 2, 3}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void shortArrayAssertionTest() {
+        Assertions.assertThat(new short[]{1, 2, 3}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new short[]{1, 2, 3}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void intArrayAssertionTest() {
+        Assertions.assertThat(new int[]{1, 2, 3}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new int[]{1, 2, 3}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void longArrayAssertionTest() {
+        Assertions.assertThat(new long[]{1L, 2L, 3L}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new long[]{1L, 2L, 3L}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void floatArrayAssertionTest() {
+        Assertions.assertThat(new float[]{1.0f, 2.0f, 3.0f}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new float[]{1.0f, 2.0f, 3.0f}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void doubleArrayAssertionTest() {
+        Assertions.assertThat(new double[]{1.0, 2.0, 3.0}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new double[]{1.0, 2.0, 3.0}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void booleanArrayAssertionTest() {
+        Assertions.assertThat(new boolean[]{true, true, false}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new boolean[]{true, true, false}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void charArrayAssertionTest() {
+        Assertions.assertThat(new char[]{'1', '2', '3'}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new char[]{'1', '2', '3'}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void objectArrayAssertionTest() {
+        Assertions.assertThat(new Object[]{"1", "2", "3"}).hasLength(3);
+
+        try {
+            Assertions.assertThat(new Object[]{"1", "2", "3"}).hasLength(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void classAssertionTest() {
+        Assertions.assertThat(String.class).isSubtypeOf(Object.class);
+
+        try {
+            Assertions.assertThat(String.class).isSubtypeOf(StringBuilder.class);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Value should be the subtype of the expected value. Expected:<java.lang.StringBuilder> but was:<java.lang.String>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void charSequenceAssertionTest() {
+        Assertions.assertThat(new StringBuilder("test")).hasLength(4);
+
+        try {
+            Assertions.assertThat(new StringBuilder("test")).hasLength(5);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<5> but was:<4>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void stringAssertionTest() {
+        Assertions.assertThat("test").hasLength(4);
+
+        try {
+            Assertions.assertThat("test").hasLength(5);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<5> but was:<4>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void comparableAssertionTest() {
+        Comparable<?> comparable = Integer.valueOf("5");
+        Assertions.assertThat(comparable).isEqualTo(5);
+
+        try {
+            Assertions.assertThat(comparable).isEqualTo(6);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<6> but was:<5>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void iterableAssertionTest() {
+        Iterable<?> iterable = Arrays.asList("1", "2", "3");
+        Assertions.assertThat(iterable).hasSize(3);
+
+        try {
+            Assertions.assertThat(iterable).hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void throwableAssertionTest() {
+        Assertions.assertThat(new AssertionError("error")).isInstanceOf(Error.class);
+
+        try {
+            Assertions.assertThat(new AssertionError("error")).isInstanceOf(RuntimeException.class);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Value should be the subtype of the expected value. Expected:<java.lang.RuntimeException> but was:<java.lang.AssertionError>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void collectionAssertionTest() {
+        Collection<?> collection = Arrays.asList("1", "2", "3");
+        Assertions.assertThat(collection).hasSize(3);
+
+        try {
+            Assertions.assertThat(collection).hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void iteratorAssertionTest() {
+        Collection<?> collection = Arrays.asList("1", "2", "3");
+        Assertions.assertThat(collection.iterator()).hasSize(3);
+
+        try {
+            Assertions.assertThat(collection.iterator()).hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void listAssertionTest() {
+        List<?> list = Arrays.asList("1", "2", "3");
+        Assertions.assertThat(list).hasSize(3);
+
+        try {
+            Assertions.assertThat(list).hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void setAssertionTest() {
+        Set<?> set = new HashSet<>(Arrays.asList("1", "2", "3"));
+        Assertions.assertThat(set).hasSize(3);
+
+        try {
+            Assertions.assertThat(set).hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void mapAssertionTest() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "val1");
+        map.put("2", "val2");
+        map.put("3", "val3");
+        Assertions.assertThat(map).toKeys().hasSize(3);
+
+        try {
+            Assertions.assertThat(map).toKeys().hasSize(4);
+            Assertions.fail("Assertions test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Values should be the same. Expected:<4> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link Assertions} class test.
+     */
+    @Test
+    public void failTest() {
+        try {
+            Assertions.fail("fail a test");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("fail a test");
+        }
+    }
+
+}
