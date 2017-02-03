2ba1b2087c9c2322efcec3964ea967731708cefb
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-02-03 07:13:23 +0300

    Добавил javadoc для пакета

--- src/main/java/ru/d_shap/assertions/package-info.java
+++ src/main/java/ru/d_shap/assertions/package-info.java
@@ -19,7 +19,44 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * <p>
- * Assertion framework for human readable testing.
+ * Classes provides facilities to make the unit test assertions more readable.
  * </p>
+ * <p>
+ * The following example shows some assertions for the primitive data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(Math.max(5, 10)).isEqualTo(10);
+ * assertThat(Math.pow(2.0, 10.0)).isGreaterThan(1000.0);
+ * }</pre>
+ * <p>
+ * The following example shows some assertions for the String:
+ * </p>
+ * <pre>{@code
+ * assertThat("value").isNotBlank();
+ * assertThat("value").isEqualTo("value");
+ * assertThat("value").containsIgnoreCase("ALU");
+ * assertThat("value").isPartOf("Some value.");
+ * assertThat("value").matches("\\w+");
+ * }</pre>
+ * <p>
+ * The following example shows some assertions for the collection data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(Arrays.asList("val1", "val2", "val3")).isNotEmpty();
+ * assertThat(Arrays.asList("val1", "val2", "val3")).hasSize(3);
+ * assertThat(Arrays.asList("val1", "val2", "val3")).containsAny("val2", "val4");
+ * assertThat(Arrays.asList("val1", "val2", "val3")).doesNotContain("test");
+ *
+ * assertThat(new HashMap<>()).isEmpty();
+ *
+ * Map<String, String> map = new HashMap<>();
+ * map.put("key1", "value1");
+ * map.put("key2", "value2");
+ * map.put("key3", "value3");
+ * assertThat(map).hasSize(3);
+ * assertThat(map).containsEntry("key2", "value2");
+ * assertThat(map).doesNotContainEntry("key", "value");
+ * assertThat(map).containsAllKeys("key1", "key2", "key3");
+ * }</pre>
  */
 package ru.d_shap.assertions;
