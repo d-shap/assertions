903667090b43c1e8fdd9a4c854b69d496614e530
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-02-10 22:31:41 +0300

    Перегрузил методы для массива int

--- src/main/java/ru/d_shap/assertions/array/ByteArrayAssertion.java
+++ src/main/java/ru/d_shap/assertions/array/ByteArrayAssertion.java
@@ -75,6 +75,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      *
      * @param expected the expected values.
      */
+    public final void containsAll(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAll(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values.
+     *
+     * @param expected the expected values.
+     */
     public final void containsAll(final Iterable<Byte> expected) {
         doContainsAll(expected);
     }
@@ -85,6 +95,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      * @param expected the expected values.
      */
     public final void containsAllInOrder(final byte... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAllInOrder(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAllInOrder(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsAllInOrder(createArgumentArray(expected));
     }
@@ -113,6 +133,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      *
      * @param expected the expected values.
      */
+    public final void containsExactly(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsExactly(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly.
+     *
+     * @param expected the expected values.
+     */
     public final void containsExactly(final Iterable<Byte> expected) {
         doContainsExactly(expected);
     }
@@ -123,6 +153,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      * @param expected the expected values.
      */
     public final void containsExactlyInOrder(final byte... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsExactlyInOrder(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactlyInOrder(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsExactlyInOrder(createArgumentArray(expected));
     }
@@ -151,6 +191,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      *
      * @param expected the expected values.
      */
+    public final void containsAny(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAny(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains any of the expected values.
+     *
+     * @param expected the expected values.
+     */
     public final void containsAny(final Iterable<Byte> expected) {
         doContainsAny(expected);
     }
@@ -161,6 +211,16 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
      * @param expected the expected values.
      */
     public final void containsNone(final byte... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsNone(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value does NOT contain any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsNone(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsNone(createArgumentArray(expected));
     }
@@ -188,6 +248,14 @@ public class ByteArrayAssertion extends BaseArrayAssertion<Byte> {
         Byte[] result = new Byte[array.length];
         for (int i = 0; i < array.length; i++) {
             result[i] = array[i];
+        }
+        return result;
+    }
+
+    private Byte[] createArgumentArray(final int... array) {
+        Byte[] result = new Byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = (byte) array[i];
         }
         return result;
     }
--- src/main/java/ru/d_shap/assertions/array/ShortArrayAssertion.java
+++ src/main/java/ru/d_shap/assertions/array/ShortArrayAssertion.java
@@ -75,6 +75,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      *
      * @param expected the expected values.
      */
+    public final void containsAll(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAll(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values.
+     *
+     * @param expected the expected values.
+     */
     public final void containsAll(final Iterable<Short> expected) {
         doContainsAll(expected);
     }
@@ -85,6 +95,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      * @param expected the expected values.
      */
     public final void containsAllInOrder(final short... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAllInOrder(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAllInOrder(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsAllInOrder(createArgumentArray(expected));
     }
@@ -113,6 +133,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      *
      * @param expected the expected values.
      */
+    public final void containsExactly(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsExactly(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly.
+     *
+     * @param expected the expected values.
+     */
     public final void containsExactly(final Iterable<Short> expected) {
         doContainsExactly(expected);
     }
@@ -123,6 +153,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      * @param expected the expected values.
      */
     public final void containsExactlyInOrder(final short... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsExactlyInOrder(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactlyInOrder(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsExactlyInOrder(createArgumentArray(expected));
     }
@@ -151,6 +191,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      *
      * @param expected the expected values.
      */
+    public final void containsAny(final int... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsAny(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value contains any of the expected values.
+     *
+     * @param expected the expected values.
+     */
     public final void containsAny(final Iterable<Short> expected) {
         doContainsAny(expected);
     }
@@ -161,6 +211,16 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
      * @param expected the expected values.
      */
     public final void containsNone(final short... expected) {
+        checkArgumentIsNotNull(expected);
+        doContainsNone(createArgumentArray(expected));
+    }
+
+    /**
+     * Check if the actual value does NOT contain any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsNone(final int... expected) {
         checkArgumentIsNotNull(expected);
         doContainsNone(createArgumentArray(expected));
     }
@@ -188,6 +248,14 @@ public class ShortArrayAssertion extends BaseArrayAssertion<Short> {
         Short[] result = new Short[array.length];
         for (int i = 0; i < array.length; i++) {
             result[i] = array[i];
+        }
+        return result;
+    }
+
+    private Short[] createArgumentArray(final int... array) {
+        Short[] result = new Short[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = (short) array[i];
         }
         return result;
     }
