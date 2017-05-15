26203df378ab7b4aae89b6f4dee0ec01fbb59b13
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2017-05-15 22:56:28 +0300

    поправил текст лицензии

--- src/main/java/ru/d_shap/assertions/collection/MapAssertion.java
+++ src/main/java/ru/d_shap/assertions/collection/MapAssertion.java
@@ -2,7 +2,7 @@
 // Assertions framework provides facilities for unit testing.
 // Copyright (C) 2017 Dmitry Shapovalov.
 //
-// This file is part of Assertions framework.
+// This file is part of assertions framework.
 //
 // Assertions framework is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
