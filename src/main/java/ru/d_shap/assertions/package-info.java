5fae3d0e012be245a7697b1c4d3f74ca4bdda053
Dmitry@Dmitry-PC <Dmitry@Dmitry-PC>
2017-09-19 22:51:07 +0300

    Добавил примеры всех утверждений в javadoc

--- src/main/java/ru/d_shap/assertions/package-info.java
+++ src/main/java/ru/d_shap/assertions/package-info.java
@@ -19,44 +19,104 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * <p>
- * Classes provides facilities to make the unit test assertions more readable.
+ * Assertions framework provides facilities for the unit testing. Framework allows checking of conditions
+ * via existing assertion classes. This assertion classes make the tests more readable, understandable and maintainable.
+ * Also this assertion classes reduce the amount of code needed to perform some complicated checks.
  * </p>
  * <p>
- * The following example shows some assertions for the primitive data types:
+ * Import declaration for all the following examples:
  * </p>
  * <pre>{@code
- * assertThat(Math.max(5, 10)).isEqualTo(10);
- * assertThat(Math.pow(2.0, 10.0)).isGreaterThan(1000.0);
+ * import static ru.d_shap.assertions.Assertions.assertThat;
  * }</pre>
  * <p>
- * The following example shows some assertions for the String:
+ * Assertion examples for the primitive data types:
  * </p>
  * <pre>{@code
- * assertThat("value").isNotBlank();
- * assertThat("value").isEqualTo("value");
- * assertThat("value").containsIgnoreCase("ALU");
- * assertThat("value").isPartOf("Some value.");
- * assertThat("value").matches("\\w+");
+ * assertThat(booleanValue).isTrue();
+ * assertThat(byteValue).isEqualTo(5);
+ * assertThat(shortValue).isNotEqualTo(10);
+ * assertThat(intValue).isGreaterThan(0);
+ * assertThat(longValue).isInRange(1L, 10L);
+ * assertThat(floatValue).isNotNaN();
+ * assertThat(doubleValue).isEqualTo(5.0, 0.001);
+ * assertThat(charValue).isAlphabetic();
  * }</pre>
  * <p>
- * The following example shows some assertions for the collection data types:
+ * Assertion examples for the core data types:
  * </p>
  * <pre>{@code
- * assertThat(Arrays.asList("val1", "val2", "val3")).isNotEmpty();
- * assertThat(Arrays.asList("val1", "val2", "val3")).hasSize(3);
- * assertThat(Arrays.asList("val1", "val2", "val3")).containsAny("val2", "val4");
- * assertThat(Arrays.asList("val1", "val2", "val3")).doesNotContain("test");
+ * assertThat(objectValue).isNotNull();
+ * assertThat(objectValue).isNotSameAs(new Object());
+ * assertThat(objectValue).isInstanceOf(Number.class);
  *
- * assertThat(new HashMap<>()).isEmpty();
+ * assertThat(stringValue).isNotBlank();
+ * assertThat(stringValue).isEqualTo("value");
+ * assertThat(stringValue).containsIgnoreCase("ALU");
+ * assertThat(stringValue).isPartOf("Some value");
+ * assertThat(stringValue).matches("\\w+");
  *
- * Map<String, String> map = new HashMap<>();
- * map.put("key1", "value1");
- * map.put("key2", "value2");
- * map.put("key3", "value3");
- * assertThat(map).hasSize(3);
- * assertThat(map).containsEntry("key2", "value2");
- * assertThat(map).doesNotContainEntry("key", "value");
- * assertThat(map).containsAllKeys("key1", "key2", "key3");
+ * assertThat(classValue).isSubtypeOf(Number.class);
+ * assertThat(classValue).hasOnePrivateConstructor();
+ * assertThat(classValue).asEnum().hasValueCount(5);
+ *
+ * assertThat(throwableValue).hasMessage("Exception");
+ * }</pre>
+ * <p>
+ * Assertion examples for the array data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(booleanArrayValue).isEmpty();
+ * assertThat(byteArrayValue).hasLength(3);
+ * assertThat(shortArrayValue).contains(5);
+ * assertThat(intArrayValue).containsAll(5, 10, 15);
+ * assertThat(longArrayValue).containsAny(1L, 2L, 3L);
+ * assertThat(floatArrayValue).containsNone(10.0f, 20.0f);
+ * assertThat(charArrayValue).doesNotContain('c');
+ * assertThat(objectArrayValue).containsAllInOrder("val1", "val2", "val3");
+ * }</pre>
+ * <p>
+ * Assertion examples for the collection data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(listValue).isNotEmpty();
+ * assertThat(listValue).hasSize(3);
+ * assertThat(listValue).containsAll("val1", "val2", "val3");
+ * assertThat(listValue).containsAny("val2", "val4");
+ * assertThat(listValue).containsNone("val7", "val5");
+ *
+ * assertThat(setValue).isEmpty();
+ * assertThat(setValue).containsExactly("val1", "val3", "val5");
+ *
+ * assertThat(mapValue).isNullOrEmpty();
+ * assertThat(mapValue).hasSize(5);
+ * assertThat(mapValue).containsAnyKey("key1", "key2", "key3");
+ * assertThat(mapValue).containsEntry("key", "value");
+ * assertThat(mapValue).containsNone(expectedMapValue);
+ * }</pre>
+ * <p>
+ * Assertion examples for the io data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(inputStreamValue).isNextBytesEqualTo(1, 2, 3);
+ * assertThat(inputStreamValue).isAllBytesEqualTo(10, 100, 15);
+ * assertThat(inputStreamValue).isCompleted();
+ *
+ * assertThat(readerValue).isNextCharsEqualTo('v', 'a', 'l', '1');
+ * assertThat(readerValue).isAllCharsEqualTo('v', 'a', 'l', '2');
+ * assertThat(readerValue).isCompleted();
+ * }</pre>
+ * <p>
+ * Assertion examples for the nio data types:
+ * </p>
+ * <pre>{@code
+ * assertThat(byteBufferValue).hasCapacity(10);
+ * assertThat(shortBufferValue).hasLimit(5);
+ * assertThat(intBufferValue).hasPosition(7);
+ * assertThat(longBufferValue).contains(4L);
+ * assertThat(floatBufferValue).containsExactlyInOrder(1.0f, 2.0f, 1.0f, 2.0f, 3.0f);
+ * assertThat(doubleBufferValue).rewindAndContainsAll(5.0, 10.0, 15.0);
+ * assertThat(charBufferValue).rewindAndContainsNone('a', 'b', 'c');
  * }</pre>
  */
 package ru.d_shap.assertions;
