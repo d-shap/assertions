738cdfaae575f58607f33b8b8d036fcfafdca375
Dmitry@Dmitry-PC <Dmitry@Dmitry-PC>
2017-10-22 16:18:33 +0300

    Изменил метод asString

--- src/main/java/ru/d_shap/assertions/io/InputStreamAssertion.java
+++ src/main/java/ru/d_shap/assertions/io/InputStreamAssertion.java
@@ -159,7 +159,11 @@ public class InputStreamAssertion extends ReferenceAssertion {
 
     @Override
     protected final String asString(final Object value, final boolean actual) {
-        return null;
+        if (value == null) {
+            return null;
+        } else {
+            return value.toString();
+        }
     }
 
 }
