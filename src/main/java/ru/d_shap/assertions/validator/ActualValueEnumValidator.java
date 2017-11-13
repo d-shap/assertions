3543692564385400be512dfcbfae2ee1ffbf6692
Dmitry@Dmitry-PC <Dmitry@Dmitry-PC>
2017-11-13 23:08:17 +0300

    Валидатор, проверяющий что класс является перечислением

---
+++ src/main/java/ru/d_shap/assertions/validator/ActualValueEnumValidator.java
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.validator;
+
+/**
+ * Validator for the actual value. Check if the actual value is the enum class.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ActualValueEnumValidator implements ActualValueValidator {
+
+    /**
+     * Create new object.
+     */
+    public ActualValueEnumValidator() {
+        super();
+    }
+
+    @Override
+    public boolean isValid(final Object actual) {
+        return actual instanceof Class<?> && Enum.class.isAssignableFrom((Class<?>) actual);
+    }
+
+}
