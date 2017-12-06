67a34b8ed887795a2f32fdbc7140f3c767064ec5
Dmitry@Dmitry-PC <Dmitry@Dmitry-PC>
2017-12-06 06:54:31 +0300

    Изменил базовый класс, реализовал утверждения

--- src/main/java/ru/d_shap/assertions/collection/SetAssertion.java
+++ src/main/java/ru/d_shap/assertions/collection/SetAssertion.java
@@ -19,27 +19,291 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.assertions.collection;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-import ru.d_shap.assertions.validator.ActualValueClassValidator;
-import ru.d_shap.assertions.validator.ActualValueValidator;
+import ru.d_shap.assertions.Messages;
+import ru.d_shap.assertions.Raw;
+import ru.d_shap.assertions.ReferenceAssertion;
+import ru.d_shap.assertions.ValueConverter;
+import ru.d_shap.assertions.primitive.IntAssertion;
 
 /**
  * Assertions for the set.
  *
- * @param <T> the set element type.
+ * @param <E> the generic type of the set element.
  * @author Dmitry Shapovalov
  */
-public class SetAssertion<T> extends CollectionAssertion<T> {
-
-    private static final ActualValueValidator ACTUAL_VALUE_CLASS_VALIDATOR = new ActualValueClassValidator(Set.class);
+public class SetAssertion<E> extends ReferenceAssertion<Set<E>> {
 
     /**
      * Create new object.
      */
     public SetAssertion() {
         super();
-        addActualValueValidator(ACTUAL_VALUE_CLASS_VALIDATOR);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected final Class<Set<E>> getActualValueClass() {
+        return (Class<Set<E>>) getRawActualValueClass();
+    }
+
+    private Class<?> getRawActualValueClass() {
+        return Set.class;
+    }
+
+    /**
+     * Check if the actual value is empty.
+     */
+    public final void isEmpty() {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (!getActual().isEmpty()) {
+            throw createAssertionErrorWithActual(Messages.Fail.IS_EMPTY);
+        }
+    }
+
+    /**
+     * Check if the actual value is null or empty.
+     */
+    public final void isNullOrEmpty() {
+        checkInitialized();
+        if (getActual() != null && !getActual().isEmpty()) {
+            throw createAssertionErrorWithActual(Messages.Fail.IS_NULL_OR_EMPTY);
+        }
+    }
+
+    /**
+     * Check if the actual value is NOT empty.
+     */
+    public final void isNotEmpty() {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (getActual().isEmpty()) {
+            throw createAssertionError(Messages.Fail.IS_NOT_EMPTY);
+        }
+    }
+
+    /**
+     * Check if the actual value contains the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void contains(final E expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (!getActual().contains(expected)) {
+            throw createAssertionErrorWithActual(Messages.Fail.CONTAINS, expected);
+        }
+    }
+
+    /**
+     * Check if the actual value does NOT contain the expected value.
+     *
+     * @param expected the expected value.
+     */
+    public final void doesNotContain(final E expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (getActual().contains(expected)) {
+            throw createAssertionErrorWithActual(Messages.Fail.DOES_NOT_CONTAIN, expected);
+        }
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsAll(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        checkArgumentIsNotEmptyTrue(expected.length == 0);
+        createListAssertion().containsAll(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAll(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        checkArgumentIsNotEmptyTrue(expectedArray.length == 0);
+        createListAssertion().containsAll(expectedArray);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsAllInOrder(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        checkArgumentIsNotEmptyTrue(expected.length == 0);
+        createListAssertion().containsAllInOrder(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAllInOrder(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        checkArgumentIsNotEmptyTrue(expectedArray.length == 0);
+        createListAssertion().containsAllInOrder(expectedArray);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsExactly(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        createListAssertion().containsExactly(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactly(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        createListAssertion().containsExactly(expectedArray);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsExactlyInOrder(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        createListAssertion().containsExactlyInOrder(expected);
+    }
+
+    /**
+     * Check if the actual value contains all of the expected values exactly in the specified order.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsExactlyInOrder(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        createListAssertion().containsExactlyInOrder(expectedArray);
+    }
+
+    /**
+     * Check if the actual value contains any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsAny(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        checkArgumentIsNotEmptyFalse(expected.length == 0);
+        createListAssertion().containsAny(expected);
+    }
+
+    /**
+     * Check if the actual value contains any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsAny(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        checkArgumentIsNotEmptyFalse(expectedArray.length == 0);
+        createListAssertion().containsAny(expectedArray);
+    }
+
+    /**
+     * Check if the actual value does NOT contain any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    @SafeVarargs
+    public final void containsNone(final E... expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        checkArgumentIsNotEmptyTrue(expected.length == 0);
+        createListAssertion().containsNone(expected);
+    }
+
+    /**
+     * Check if the actual value does NOT contain any of the expected values.
+     *
+     * @param expected the expected values.
+     */
+    public final void containsNone(final Iterable<E> expected) {
+        checkInitialized();
+        checkActualIsNotNull();
+        checkArgumentIsNotNull(expected);
+        E[] expectedArray = ValueConverter.toObjectArray(expected);
+        checkArgumentIsNotEmptyTrue(expectedArray.length == 0);
+        createListAssertion().containsNone(expectedArray);
+    }
+
+    /**
+     * Make assertion about the actual value size.
+     *
+     * @return the assertion.
+     */
+    public final IntAssertion toSize() {
+        checkInitialized();
+        checkActualIsNotNull();
+        return initializeAssertion(Raw.intAssertion(), getActual().size(), Messages.Check.ACTUAL_VALUE_SIZE);
+    }
+
+    /**
+     * Check if the actual value size is equal to the expected size.
+     *
+     * @param expected the expected size.
+     */
+    public final void hasSize(final int expected) {
+        toSize().isEqualTo(expected);
+    }
+
+    private ListAssertion<E> createListAssertion() {
+        List<E> list = new ArrayList<>(getActual());
+        return initializeAssertion(Raw.<E>listAssertion(), list);
+    }
+
+    @Override
+    protected final String asString(final Object value) {
+        return value.toString();
     }
 
 }
