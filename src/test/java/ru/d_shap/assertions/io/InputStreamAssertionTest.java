14c5039f8699f2629bcd140e12aae74905bf2641
Dmitry Shapovalov <shenf@mail.ru>
2018-05-31 15:36:51 +0300

    Добавил тест для проверки hasAvailable

--- src/test/java/ru/d_shap/assertions/io/InputStreamAssertionTest.java
+++ src/test/java/ru/d_shap/assertions/io/InputStreamAssertionTest.java
@@ -881,10 +881,11 @@ public final class InputStreamAssertionTest extends AssertionTest {
      */
     @Test
     public void toAvailableTest() {
-        initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2, 3})).toAvailable().isEqualTo(3);
+        initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2})).toAvailable().isEqualTo(2);
         initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{0, 0, 0, 0, 0})).toAvailable().isEqualTo(5);
         initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[0])).toAvailable().isEqualTo(0);
         ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{1, 2, 3});
+        initialize(Raw.inputStreamAssertion(), bais).toAvailable().isEqualTo(3);
         Assertions.assertThat(bais.read()).isEqualTo(1);
         initialize(Raw.inputStreamAssertion(), bais).toAvailable().isEqualTo(2);
         Assertions.assertThat(bais.read()).isEqualTo(2);
@@ -920,6 +921,57 @@ public final class InputStreamAssertionTest extends AssertionTest {
         }
         try {
             initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2, 3}), "Message").toAvailable().isEqualTo(2);
+            Assertions.fail("InputStreamAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Message.\n\tCheck actual value's available.\n\tActual and expected values should be the same.\n\tExpected:<2> but was:<3>");
+        }
+    }
+
+    /**
+     * {@link InputStreamAssertion} class test.
+     */
+    @Test
+    public void hasAvailableTest() {
+        initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2})).hasAvailable(2);
+        initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{0, 0, 0, 0, 0})).hasAvailable(5);
+        initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[0])).hasAvailable(0);
+        ByteArrayInputStream bais = new ByteArrayInputStream(new byte[]{1, 2, 3});
+        initialize(Raw.inputStreamAssertion(), bais).hasAvailable(3);
+        Assertions.assertThat(bais.read()).isEqualTo(1);
+        initialize(Raw.inputStreamAssertion(), bais).hasAvailable(2);
+        Assertions.assertThat(bais.read()).isEqualTo(2);
+        initialize(Raw.inputStreamAssertion(), bais).hasAvailable(1);
+        Assertions.assertThat(bais.read()).isEqualTo(3);
+        initialize(Raw.inputStreamAssertion(), bais).hasAvailable(0);
+        Assertions.assertThat(bais.read()).isLessThan(0);
+        initialize(Raw.inputStreamAssertion(), bais).hasAvailable(0);
+
+        try {
+            Raw.inputStreamAssertion().hasAvailable(0);
+            Assertions.fail("InputStreamAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Assertion should be initialized.");
+        }
+        try {
+            initialize(Raw.inputStreamAssertion(), null).hasAvailable(0);
+            Assertions.fail("InputStreamAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Actual value should not be null.");
+        }
+        try {
+            initialize(Raw.inputStreamAssertion(), null, "Message").hasAvailable(0);
+            Assertions.fail("InputStreamAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Message.\n\tActual value should not be null.");
+        }
+        try {
+            initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2, 3})).hasAvailable(2);
+            Assertions.fail("InputStreamAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Check actual value's available.\n\tActual and expected values should be the same.\n\tExpected:<2> but was:<3>");
+        }
+        try {
+            initialize(Raw.inputStreamAssertion(), new ByteArrayInputStream(new byte[]{1, 2, 3}), "Message").hasAvailable(2);
             Assertions.fail("InputStreamAssertion test fail");
         } catch (AssertionError ex) {
             Assertions.assertThat(ex).hasMessage("Message.\n\tCheck actual value's available.\n\tActual and expected values should be the same.\n\tExpected:<2> but was:<3>");
