891bb97fbbbb55ed0945519b5bfb23b19b95e488
Dmitry Shapovalov <shenf@mail.ru>
2018-06-06 15:54:29 +0300

    Добавил конвертер в строку для boolean

---
+++ src/main/java/ru/d_shap/assertions/primitive/BooleanAsStringConverter.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.primitive;
+
+import ru.d_shap.assertions.utils.AsStringConverter;
+
+/**
+ * Value converter to the string representation for the boolean.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class BooleanAsStringConverter implements AsStringConverter {
+
+    /**
+     * Create new object.
+     */
+    public BooleanAsStringConverter() {
+        super();
+    }
+
+    @Override
+    public boolean canConvert(final Object value) {
+        return value instanceof Boolean;
+    }
+
+    @Override
+    public String asString(final Object value) {
+        return value.toString();
+    }
+
+}
