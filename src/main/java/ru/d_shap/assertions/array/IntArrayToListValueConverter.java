5a772a6fdac659635e60b826782a5e0bfa6e8e4d
Dmitry Shapovalov <shenf@mail.ru>
2018-06-07 15:54:49 +0300

    Добавил конвертер в строку для массива int

---
+++ src/main/java/ru/d_shap/assertions/array/IntArrayToListValueConverter.java
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.d_shap.assertions.BaseValueConverter;
+
+/**
+ * Value converter for the int array to list.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class IntArrayToListValueConverter extends BaseValueConverter {
+
+    /**
+     * Create new object.
+     */
+    public IntArrayToListValueConverter() {
+        super();
+    }
+
+    @Override
+    protected final Class<?> getValueClass() {
+        return int[].class;
+    }
+
+    @Override
+    protected final Class<?> getTargetClass() {
+        return List.class;
+    }
+
+    @Override
+    protected final Object convert(final Object value) {
+        List<Integer> list = new ArrayList<>(((int[]) value).length);
+        for (int element : (int[]) value) {
+            list.add(element);
+        }
+        return list;
+    }
+
+}
