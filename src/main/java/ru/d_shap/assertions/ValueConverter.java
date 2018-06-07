8d9f1f8e5e94c630a2d2cb739b8620e500c3379a
Dmitry Shapovalov <shenf@mail.ru>
2018-06-07 15:21:41 +0300

    Класс для преобразования значений из одного типа в другой

---
+++ src/main/java/ru/d_shap/assertions/ValueConverter.java
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.d_shap.assertions.array.BooleanArrayToListValueConverter;
+
+/**
+ * Class to convert value to the value with the target class.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class ValueConverter {
+
+    private static final List<BaseValueConverter> CONVERTERS;
+
+    static {
+        CONVERTERS = new ArrayList<>(1);
+        CONVERTERS.add(new BooleanArrayToListValueConverter());
+    }
+
+    private ValueConverter() {
+        super();
+    }
+
+    static Object convert(final Object value, final Class<?> targetClass) {
+        if (value == null) {
+            return null;
+        }
+        Class<?> valueClass = value.getClass();
+        for (int i = 0; i < CONVERTERS.size(); i++) {
+            BaseValueConverter converter = CONVERTERS.get(i);
+            if (converter.getValueClass().isAssignableFrom(valueClass) && converter.getTargetClass().isAssignableFrom(targetClass)) {
+                return converter.convert(targetClass);
+            }
+        }
+        return value.toString();
+    }
+
+}
