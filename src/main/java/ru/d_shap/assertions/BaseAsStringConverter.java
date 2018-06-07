4dae3c7d309cfc6ae014b96a77c6a3a11ef0de09
Dmitry Shapovalov <shenf@mail.ru>
2018-06-07 10:46:32 +0300

    Базовый класс для всех классов преобразования в строку

---
+++ src/main/java/ru/d_shap/assertions/BaseAsStringConverter.java
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions;
+
+/**
+ * Base class for all value to string converters.
+ *
+ * @param <T> the generic type of the value.
+ * @author Dmitry Shapovalov
+ */
+public abstract class BaseAsStringConverter<T> {
+
+    /**
+     * Create new object.
+     */
+    protected BaseAsStringConverter() {
+        super();
+    }
+
+    /**
+     * Get the value class.
+     *
+     * @return the value class.
+     */
+    protected abstract Class<T> getValueClass();
+
+    /**
+     * Get the string representation of the value.
+     *
+     * @param value the value.
+     * @return the string representation of the value.
+     */
+    protected abstract String asString(Object value);
+
+    /**
+     * Get the string representation of the child value.
+     *
+     * @param value the child value.
+     * @return the string representation of the child value.
+     */
+    protected final String getChildValueAsString(final Object value) {
+        return AsStringConverter.asString(value);
+    }
+
+}
