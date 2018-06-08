24acfa140db828aa67946f7f1a5e28e747fb78a4
Dmitry Shapovalov <shenf@mail.ru>
2018-06-08 14:07:12 +0300

    Добавил конвертер из int в byte

---
+++ src/main/java/ru/d_shap/assertions/primitive/IntToByteValueConverter.java
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.primitive;
+
+import ru.d_shap.assertions.BaseValueConverter;
+
+/**
+ * Value converter for the int to byte.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class IntToByteValueConverter extends BaseValueConverter {
+
+    /**
+     * Create new object.
+     */
+    public IntToByteValueConverter() {
+        super();
+    }
+
+    @Override
+    protected final Class<?> getValueClass() {
+        return Integer.class;
+    }
+
+    @Override
+    protected final Class<?> getTargetClass() {
+        return Byte.class;
+    }
+
+    @Override
+    protected final boolean canConvert(final Object value) {
+        int code = (int) value;
+        byte symbol = (byte) code;
+        return code == symbol;
+    }
+
+    @Override
+    protected final Object convert(final Object value) {
+        return (byte) (int) value;
+    }
+
+}
