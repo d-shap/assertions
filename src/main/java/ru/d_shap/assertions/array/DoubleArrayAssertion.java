df3a788d7233ffccb7996a5e27b62db9f4982330
Dmitry Shapovalov <shenf@mail.ru>
2018-06-09 15:53:20 +0300

    Рефакторинг кода

--- src/main/java/ru/d_shap/assertions/array/DoubleArrayAssertion.java
+++ src/main/java/ru/d_shap/assertions/array/DoubleArrayAssertion.java
@@ -21,14 +21,18 @@ package ru.d_shap.assertions.array;
 
 import java.util.List;
 
-import ru.d_shap.assertions.utils.ValueConverter;
+import ru.d_shap.assertions.Messages;
+import ru.d_shap.assertions.Raw;
+import ru.d_shap.assertions.ReferenceAssertion;
+import ru.d_shap.assertions.collection.ListAssertion;
+import ru.d_shap.assertions.primitive.IntAssertion;
 
 /**
  * Assertions for the double array.
  *
  * @author Dmitry Shapovalov
  */
-public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
+public class DoubleArrayAssertion extends ReferenceAssertion<double[]> {
 
     /**
      * Create new object.
@@ -43,12 +47,46 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
     }
 
     /**
+     * Check if the actual value is empty.
+     */
+    public final void isEmpty() {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (getActual().length != 0) {
+            throw createAssertionErrorWithActual(Messages.Fail.IS_EMPTY);
+        }
+    }
+
+    /**
+     * Check if the actual value is null or empty.
+     */
+    public final void isNullOrEmpty() {
+        checkInitialized();
+        if (getActual() != null && getActual().length != 0) {
+            throw createAssertionErrorWithActual(Messages.Fail.IS_NULL_OR_EMPTY);
+        }
+    }
+
+    /**
+     * Check if the actual value is NOT empty.
+     */
+    public final void isNotEmpty() {
+        checkInitialized();
+        checkActualIsNotNull();
+        if (getActual().length == 0) {
+            throw createAssertionError(Messages.Fail.IS_NOT_EMPTY);
+        }
+    }
+
+    /**
      * Check if the actual value contains the expected value.
      *
      * @param expected the expected value.
      */
     public final void contains(final double expected) {
-        doContains(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().contains(expected);
     }
 
     /**
@@ -57,7 +95,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected value.
      */
     public final void doesNotContain(final double expected) {
-        doDoesNotContain(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().doesNotContain(expected);
     }
 
     /**
@@ -69,7 +109,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsAll(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsAll(expectedList);
     }
 
     /**
@@ -78,7 +119,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsAll(final Iterable<Double> expected) {
-        doContainsAll(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsAll(expected);
     }
 
     /**
@@ -90,7 +133,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsAllInOrder(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsAllInOrder(expectedList);
     }
 
     /**
@@ -99,7 +143,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsAllInOrder(final Iterable<Double> expected) {
-        doContainsAllInOrder(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsAllInOrder(expected);
     }
 
     /**
@@ -111,7 +157,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsExactly(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsExactly(expectedList);
     }
 
     /**
@@ -120,7 +167,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsExactly(final Iterable<Double> expected) {
-        doContainsExactly(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsExactly(expected);
     }
 
     /**
@@ -132,7 +181,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsExactlyInOrder(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsExactlyInOrder(expectedList);
     }
 
     /**
@@ -141,7 +191,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsExactlyInOrder(final Iterable<Double> expected) {
-        doContainsExactlyInOrder(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsExactlyInOrder(expected);
     }
 
     /**
@@ -153,7 +205,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsAny(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsAny(expectedList);
     }
 
     /**
@@ -162,7 +215,9 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsAny(final Iterable<Double> expected) {
-        doContainsAny(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsAny(expected);
     }
 
     /**
@@ -174,7 +229,8 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
         checkInitialized();
         checkActualIsNotNull();
         checkArgumentIsNotNull(expected);
-        doContainsNone(ValueConverter.toDoubleObjectArray(expected));
+        List<Double> expectedList = convertValue(expected, List.class);
+        createListAssertion().containsNone(expectedList);
     }
 
     /**
@@ -183,12 +239,34 @@ public class DoubleArrayAssertion extends ArrayAssertion<double[], Double> {
      * @param expected the expected values.
      */
     public final void containsNone(final Iterable<Double> expected) {
-        doContainsNone(expected);
+        checkInitialized();
+        checkActualIsNotNull();
+        createListAssertion().containsNone(expected);
     }
 
-    @Override
-    final List<Double> createList(final Object value) {
-        return ValueConverter.toDoubleList((double[]) value);
+    private ListAssertion<Double> createListAssertion() {
+        List<Double> list = convertValue(getActual(), List.class);
+        return initializeAssertion(Raw.<Double>listAssertion(), list);
+    }
+
+    /**
+     * Make assertion about the actual value's length.
+     *
+     * @return the assertion.
+     */
+    public final IntAssertion toLength() {
+        checkInitialized();
+        checkActualIsNotNull();
+        return initializeAssertion(Raw.intAssertion(), getActual().length, Messages.Check.ACTUAL_VALUE_LENGTH);
+    }
+
+    /**
+     * Check if the actual value length is equal to the expected length.
+     *
+     * @param expected the expected length.
+     */
+    public final void hasLength(final int expected) {
+        toLength().isEqualTo(expected);
     }
 
 }
