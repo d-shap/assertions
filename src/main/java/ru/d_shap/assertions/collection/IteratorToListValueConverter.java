812e276c4188f774097917e5469bebfa95ae01e0
Dmitry Shapovalov <shenf@mail.ru>
2018-06-15 12:47:11 +0300

    Добавил конвертеры значений для итератора

---
+++ src/main/java/ru/d_shap/assertions/collection/IteratorToListValueConverter.java
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import ru.d_shap.assertions.BaseValueConverter;
+
+/**
+ * Value converter from the iterator to the list.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class IteratorToListValueConverter extends BaseValueConverter {
+
+    /**
+     * Create new object.
+     */
+    public IteratorToListValueConverter() {
+        super();
+    }
+
+    @Override
+    protected Class<?> getValueClass() {
+        return Iterator.class;
+    }
+
+    @Override
+    protected Class<?> getTargetClass() {
+        return List.class;
+    }
+
+    @Override
+    protected boolean canConvert(final Object value, final Object... arguments) {
+        return true;
+    }
+
+    @Override
+    protected Object convert(final Object value, final Object... arguments) {
+        List<Object> result = new ArrayList<>();
+        int count;
+        if (arguments.length > 0) {
+            count = (Integer) arguments[0];
+        } else {
+            count = 0;
+        }
+        for (int i = 0; (count == 0 || i < count) && ((Iterator<?>) value).hasNext(); i++) {
+            Object element = ((Iterator<?>) value).next();
+            result.add(element);
+        }
+        return result;
+    }
+
+}
