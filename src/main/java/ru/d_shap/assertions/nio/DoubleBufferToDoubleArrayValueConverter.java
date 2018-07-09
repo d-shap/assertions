38fd81c22cbcb2036a32ffede033ce9c65a1f90e
Dmitry Shapovalov <shenf@mail.ru>
2018-07-09 16:57:22 +0300

    Заглушки для конвертеров значений для буферов

---
+++ src/main/java/ru/d_shap/assertions/nio/DoubleBufferToDoubleArrayValueConverter.java
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.nio;
+
+import java.nio.DoubleBuffer;
+
+import ru.d_shap.assertions.BaseValueConverter;
+import ru.d_shap.assertions.ConvertionException;
+
+/**
+ * Value converter from the double buffer to the double array.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DoubleBufferToDoubleArrayValueConverter extends BaseValueConverter {
+
+    /**
+     * Create new object.
+     */
+    public DoubleBufferToDoubleArrayValueConverter() {
+        super();
+    }
+
+    @Override
+    protected Class<?> getValueClass() {
+        return DoubleBuffer.class;
+    }
+
+    @Override
+    protected Class<?> getTargetClass() {
+        return double[].class;
+    }
+
+    @Override
+    protected boolean canConvert(final Object value, final Object... arguments) throws ConvertionException {
+        return true;
+    }
+
+    @Override
+    protected Object convert(final Object value, final Object... arguments) throws ConvertionException {
+        return null;
+    }
+
+}
