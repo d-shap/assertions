b900e66092a95f1e60036f97d08199dbb570d6b9
Dmitry Shapovalov <shenf@mail.ru>
2018-07-26 17:14:09 +0300

    Тесты для класса преобразования в строку

---
+++ src/test/java/ru/d_shap/assertions/array/DoubleArrayAsStringConverterTest.java
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.array;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.ConvertionException;
+
+/**
+ * Tests for {@link DoubleArrayAsStringConverter}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DoubleArrayAsStringConverterTest {
+
+    /**
+     * Test class constructor.
+     */
+    public DoubleArrayAsStringConverterTest() {
+        super();
+    }
+
+    /**
+     * {@link DoubleArrayAsStringConverter} class test.
+     */
+    @Test
+    public void getValueClassTest() {
+        Assertions.assertThat(new DoubleArrayAsStringConverter().getValueClass()).isEqualTo(double[].class);
+    }
+
+    /**
+     * {@link DoubleArrayAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test
+    public void asStringTest() throws ConvertionException {
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(new double[]{})).isEqualTo("[]");
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(new double[]{1.0})).isEqualTo("[1.0]");
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(new double[]{1.0, 2.0})).isEqualTo("[1.0, 2.0]");
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(new double[]{1.0, 2.0, 3.0, 4.0})).isEqualTo("[1.0, 2.0, 3.0, 4.0]");
+
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(null)).isNull();
+        Assertions.assertThat(new DoubleArrayAsStringConverter().asString(new Object())).startsWith("java.lang.Object@");
+    }
+
+}
