ef3ab738ee62c74ba970d205df54c052c96cd7a6
Dmitry Shapovalov <shenf@mail.ru>
2018-08-01 15:01:26 +0300

    Тесты для классов преобразования в строку

---
+++ src/test/java/ru/d_shap/assertions/core/IterableAsStringConverterTest.java
@@ -0,0 +1,90 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.ConvertionException;
+
+/**
+ * Tests for {@link IterableAsStringConverter}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class IterableAsStringConverterTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public IterableAsStringConverterTest() {
+        super();
+    }
+
+    /**
+     * {@link IterableAsStringConverter} class test.
+     */
+    @Test
+    public void getValueClassTest() {
+        Assertions.assertThat(new IterableAsStringConverter().getValueClass()).isEqualTo(Iterable.class);
+    }
+
+    /**
+     * {@link IterableAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test
+    public void asStringTest() throws ConvertionException {
+        Assertions.assertThat(new IterableAsStringConverter().asString(new ArrayList<String>())).isEqualTo("[]");
+        Assertions.assertThat(new IterableAsStringConverter().asString(new IterableImpl(new ArrayList<String>()))).isEqualTo("[]");
+
+        Assertions.assertThat(new IterableAsStringConverter().asString(Arrays.asList("val1", "val2", "val3"))).isEqualTo("[val1, val2, val3]");
+        Assertions.assertThat(new IterableAsStringConverter().asString(new IterableImpl(Arrays.asList("val1", "val2", "val3")))).isEqualTo("[val1, val2, val3]");
+
+        Assertions.assertThat(new IterableAsStringConverter().asString(Arrays.asList("val1", "val2", "val3", "val4", "val5"))).isEqualTo("[val1, val2, val3, val4, val5]");
+        Assertions.assertThat(new IterableAsStringConverter().asString(new IterableImpl(Arrays.asList("val1", "val2", "val3", "val4", "val5")))).isEqualTo("[val1, val2, val3, val4, val5]");
+    }
+
+    /**
+     * {@link IterableAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = NullPointerException.class)
+    public void asStringNullValueFailTest() throws ConvertionException {
+        new IterableAsStringConverter().asString(null);
+    }
+
+    /**
+     * {@link IterableAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = ClassCastException.class)
+    public void asStringWrongValueTypeFailTest() throws ConvertionException {
+        new IterableAsStringConverter().asString(new Object());
+    }
+
+}
