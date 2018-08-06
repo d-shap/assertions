3f7e6f6ee8926d112aa4468b39159f5299b639c1
Dmitry Shapovalov <shenf@mail.ru>
2018-08-06 11:24:03 +0300

    Тесты для классов преобразования в строку

---
+++ src/test/java/ru/d_shap/assertions/collection/CollectionAsStringConverterTest.java
@@ -0,0 +1,96 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.ConvertionException;
+
+/**
+ * Tests for {@link CollectionAsStringConverter}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class CollectionAsStringConverterTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public CollectionAsStringConverterTest() {
+        super();
+    }
+
+    /**
+     * {@link CollectionAsStringConverter} class test.
+     */
+    @Test
+    public void getValueClassTest() {
+        Assertions.assertThat(new CollectionAsStringConverter().getValueClass()).isEqualTo(Collection.class);
+    }
+
+    /**
+     * {@link CollectionAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test
+    public void asStringTest() throws ConvertionException {
+        List<String> list = new ArrayList<>();
+        Assertions.assertThat(new CollectionAsStringConverter().asString(list)).isEqualTo("[]");
+
+        list.add("val1");
+        Assertions.assertThat(new CollectionAsStringConverter().asString(list)).isEqualTo("[val1]");
+
+        list.add("val2");
+        list.add("val3");
+        Assertions.assertThat(new CollectionAsStringConverter().asString(list)).isEqualTo("[val1, val2, val3]");
+
+        list.add("val4");
+        list.add("val5");
+        Assertions.assertThat(new CollectionAsStringConverter().asString(list)).isEqualTo("[val1, val2, val3, val4, val5]");
+    }
+
+    /**
+     * {@link CollectionAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = NullPointerException.class)
+    public void asStringNullValueFailTest() throws ConvertionException {
+        new CollectionAsStringConverter().asString(null);
+    }
+
+    /**
+     * {@link CollectionAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = ClassCastException.class)
+    public void asStringWrongValueTypeFailTest() throws ConvertionException {
+        new CollectionAsStringConverter().asString(new Object());
+    }
+
+}
---
+++ src/test/java/ru/d_shap/assertions/collection/MapAsStringConverterTest.java
@@ -0,0 +1,95 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.ConvertionException;
+
+/**
+ * Tests for {@link MapAsStringConverter}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class MapAsStringConverterTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public MapAsStringConverterTest() {
+        super();
+    }
+
+    /**
+     * {@link MapAsStringConverter} class test.
+     */
+    @Test
+    public void getValueClassTest() {
+        Assertions.assertThat(new MapAsStringConverter().getValueClass()).isEqualTo(Map.class);
+    }
+
+    /**
+     * {@link MapAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test
+    public void asStringTest() throws ConvertionException {
+        Map<String, String> map = new LinkedHashMap<>();
+        Assertions.assertThat(new MapAsStringConverter().asString(map)).isEqualTo("{}");
+
+        map.put("key1", "val1");
+        Assertions.assertThat(new MapAsStringConverter().asString(map)).isEqualTo("{key1=val1}");
+
+        map.put("key2", "val2");
+        map.put("key3", "val3");
+        Assertions.assertThat(new MapAsStringConverter().asString(map)).isEqualTo("{key1=val1, key2=val2, key3=val3}");
+
+        map.put("key4", "val4");
+        map.put("key5", "val5");
+        Assertions.assertThat(new MapAsStringConverter().asString(map)).isEqualTo("{key1=val1, key2=val2, key3=val3, key4=val4, key5=val5}");
+    }
+
+    /**
+     * {@link MapAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = NullPointerException.class)
+    public void asStringNullValueFailTest() throws ConvertionException {
+        new MapAsStringConverter().asString(null);
+    }
+
+    /**
+     * {@link MapAsStringConverter} class test.
+     *
+     * @throws ConvertionException wrapper for exceptions, that can occur during conversion.
+     */
+    @Test(expected = ClassCastException.class)
+    public void asStringWrongValueTypeFailTest() throws ConvertionException {
+        new MapAsStringConverter().asString(new Object());
+    }
+
+}
