b8d1f6fee227f3ba5acc5bbe41297ac11f34a6c4
Dmitry Shapovalov <shenf@mail.ru>
2019-04-01 16:28:52 +0300

    Интерфейс для конвертеров в строку

---
+++ src/main/java/ru/d_shap/assertions/converter/AsStringConverterProvider.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.converter;
+
+/**
+ * Service provider interface for the value to string converter.
+ *
+ * @author Dmitry Shapovalov
+ */
+public interface AsStringConverterProvider {
+
+    /**
+     * Get the value class.
+     *
+     * @return the value class.
+     */
+    Class<?> getValueClass();
+
+    /**
+     * Get the string representation of the value.
+     *
+     * @param asStringConverter the value to string converter.
+     * @param value             the value.
+     *
+     * @return the string representation of the value.
+     *
+     * @throws ConversionException wrapper for exceptions, that can occur during conversion.
+     */
+    String asString(AsStringConverter asStringConverter, Object value) throws ConversionException;
+
+}
