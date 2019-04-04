5c53f3f3a9a9a129900268a8d5fd69f84a482ffd
Dmitry Shapovalov <shenf@mail.ru>
2019-04-04 22:08:03 +0300

    Добавил javadoc для пакета

---
+++ src/main/java/ru/d_shap/assertions/fail/package-info.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions framework provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions framework.
+//
+// Assertions framework is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions framework is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * <p>
+ * Package contains classes to describe the failure and to generate the assertion error with the fail
+ * description.
+ * </p>
+ * <p>
+ * The fail description consists of the fail description entries, describing the check.
+ * The last fail description entry can contain actual and expected value description.
+ * </p>
+ */
+package ru.d_shap.assertions.fail;
