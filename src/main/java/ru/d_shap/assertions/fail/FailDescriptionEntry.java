75a6f0de57c458652906a3d6b03e7b848c8bcf30
Dmitry Shapovalov <shenf@mail.ru>
2019-04-04 16:14:01 +0300

    Перенес в другой пакет

--- src/main/java/ru/d_shap/assertions/fail/FailDescriptionEntry.java
+++ src/main/java/ru/d_shap/assertions/fail/FailDescriptionEntry.java
@@ -17,11 +17,13 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.assertions;
+package ru.d_shap.assertions.fail;
 
 import java.text.Format;
 import java.text.MessageFormat;
 import java.util.List;
+
+import ru.d_shap.assertions.Messages;
 
 /**
  * Fail description entry.
