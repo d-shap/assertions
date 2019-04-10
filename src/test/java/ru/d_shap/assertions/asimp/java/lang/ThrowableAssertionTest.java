d64baa3e64dc159e954f3490b170806623f6dae0
Dmitry Shapovalov <shenf@mail.ru>
2019-04-10 08:59:25 +0300

    Перенес тесты в другой пакет

--- src/test/java/ru/d_shap/assertions/asimp/java/lang/ThrowableAssertionTest.java
+++ src/test/java/ru/d_shap/assertions/asimp/java/lang/ThrowableAssertionTest.java
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.assertions.core;
+package ru.d_shap.assertions.asimp.java.lang;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
