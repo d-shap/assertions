523b8f6a1cca70eb5b5e65b0771cae275aa5d123
Dmitry Shapovalov <shenf@mail.ru>
2019-04-12 16:13:15 +0300

    Перенес тесты в другой пакет

--- src/test/java/ru/d_shap/assertions/asimp/java/lang/ClassActualValueValidatorTest.java
+++ src/test/java/ru/d_shap/assertions/asimp/java/lang/ClassActualValueValidatorTest.java
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.assertions.core;
+package ru.d_shap.assertions.asimp.java.lang;
 
 import org.junit.Test;
 
@@ -25,27 +25,28 @@ import ru.d_shap.assertions.AssertionTest;
 import ru.d_shap.assertions.Assertions;
 
 /**
- * Tests for {@link EnumActualValueValidator}.
+ * Tests for {@link ClassActualValueValidator}.
  *
  * @author Dmitry Shapovalov
  */
-public final class EnumActualValueValidatorTest extends AssertionTest {
+public final class ClassActualValueValidatorTest extends AssertionTest {
 
     /**
      * Test class constructor.
      */
-    public EnumActualValueValidatorTest() {
+    public ClassActualValueValidatorTest() {
         super();
     }
 
     /**
-     * {@link EnumActualValueValidator} class test.
+     * {@link ClassActualValueValidator} class test.
      */
     @Test
     public void isValidTest() {
-        Assertions.assertThat(new EnumActualValueValidator().isValid(Values.class)).isTrue();
-        Assertions.assertThat(new EnumActualValueValidator().isValid(Object.class)).isFalse();
-        Assertions.assertThat(new EnumActualValueValidator().isValid("value")).isFalse();
+        Assertions.assertThat(new ClassActualValueValidator(String.class).isValid("value")).isTrue();
+        Assertions.assertThat(new ClassActualValueValidator(Object.class).isValid("value")).isTrue();
+        Assertions.assertThat(new ClassActualValueValidator(Integer.class).isValid("value")).isFalse();
+        Assertions.assertThat(new ClassActualValueValidator(Integer.class).isValid(new Object())).isFalse();
     }
 
 }
