39c221507d136f6fa34de72db891cb192407234c
Dmitry Shapovalov <shenf@mail.ru>
2019-04-15 17:08:10 +0300

    Перенес тесты в другой пакет

--- src/test/java/ru/d_shap/assertions/converter/ConversionExceptionTest.java
+++ src/test/java/ru/d_shap/assertions/converter/ConversionExceptionTest.java
@@ -17,11 +17,14 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.assertions;
+package ru.d_shap.assertions.converter;
 
 import java.io.IOException;
 
 import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
 
 /**
  * Tests for {@link ConversionException}.
