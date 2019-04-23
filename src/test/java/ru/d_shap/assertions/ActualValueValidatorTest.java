68d4a1ce60e898e05675ffb943deec247035f4fb
Dmitry Shapovalov <shenf@mail.ru>
2019-04-23 07:04:10 +0300

    Поправил тесты для нового api

--- src/test/java/ru/d_shap/assertions/ActualValueValidatorTest.java
+++ src/test/java/ru/d_shap/assertions/ActualValueValidatorTest.java
@@ -21,6 +21,8 @@ package ru.d_shap.assertions;
 
 import org.junit.Test;
 
+import ru.d_shap.assertions.fail.AssertionErrorBuilder;
+
 /**
  * Tests for {@link ActualValueValidator}.
  *
@@ -41,40 +43,42 @@ public final class ActualValueValidatorTest extends AssertionTest {
     @Test
     public void isValidTest() {
         ActualValueValidator actualValueValidator1 = new ActualValueValidator();
-        Assertions.assertThat(actualValueValidator1.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator1.isValid(new Object())).isTrue();
+        Assertions.assertThat(actualValueValidator1.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator1.validate(new Object())).isNull();
 
         ActualValueValidator actualValueValidator2 = new ActualValueValidator();
         actualValueValidator2.addActualValueValidator(createActualValueValidator(true));
-        Assertions.assertThat(actualValueValidator2.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator2.isValid(new Object())).isTrue();
+        Assertions.assertThat(actualValueValidator2.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator2.validate(new Object())).isNull();
 
         ActualValueValidator actualValueValidator3 = new ActualValueValidator();
         actualValueValidator3.addActualValueValidator(createActualValueValidator(true));
         actualValueValidator3.addActualValueValidator(createActualValueValidator(true));
-        Assertions.assertThat(actualValueValidator3.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator3.isValid(new Object())).isTrue();
+        Assertions.assertThat(actualValueValidator3.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator3.validate(new Object())).isNull();
 
         ActualValueValidator actualValueValidator4 = new ActualValueValidator();
         actualValueValidator4.addActualValueValidator(createActualValueValidator(true));
         actualValueValidator4.addActualValueValidator(createActualValueValidator(true));
         actualValueValidator4.addActualValueValidator(createActualValueValidator(true));
-        Assertions.assertThat(actualValueValidator4.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator4.isValid(new Object())).isTrue();
+        Assertions.assertThat(actualValueValidator4.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator4.validate(new Object())).isNull();
 
         ActualValueValidator actualValueValidator5 = new ActualValueValidator();
         actualValueValidator5.addActualValueValidator(createActualValueValidator(true));
-        actualValueValidator5.addActualValueValidator(createActualValueValidator(false));
+        ActualValueValidatorProvider actualValueValidatorProvider5 = createActualValueValidator(false);
+        actualValueValidator5.addActualValueValidator(actualValueValidatorProvider5);
         actualValueValidator5.addActualValueValidator(createActualValueValidator(true));
-        Assertions.assertThat(actualValueValidator5.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator5.isValid(new Object())).isFalse();
+        Assertions.assertThat(actualValueValidator5.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator5.validate(new Object())).isEqualTo(actualValueValidatorProvider5);
 
         ActualValueValidator actualValueValidator6 = new ActualValueValidator();
         actualValueValidator6.addActualValueValidator(createActualValueValidator(true));
         actualValueValidator6.addActualValueValidator(createActualValueValidator(true));
-        actualValueValidator6.addActualValueValidator(createActualValueValidator(false));
-        Assertions.assertThat(actualValueValidator6.isValid(null)).isTrue();
-        Assertions.assertThat(actualValueValidator6.isValid(new Object())).isFalse();
+        ActualValueValidatorProvider actualValueValidatorProvider6 = createActualValueValidator(false);
+        actualValueValidator6.addActualValueValidator(actualValueValidatorProvider6);
+        Assertions.assertThat(actualValueValidator6.validate(null)).isNull();
+        Assertions.assertThat(actualValueValidator6.validate(new Object())).isEqualTo(actualValueValidatorProvider6);
     }
 
     private ActualValueValidatorProvider createActualValueValidator(final boolean valid) {
@@ -98,6 +102,16 @@ public final class ActualValueValidatorTest extends AssertionTest {
         @Override
         public boolean isValid(final Object actual) {
             return _valid;
+        }
+
+        @Override
+        public Object getFailActualValue(final Object actual) {
+            return actual;
+        }
+
+        @Override
+        public void addFailMessage(final AssertionErrorBuilder assertionErrorBuilder) {
+            // Ignore
         }
 
     }
