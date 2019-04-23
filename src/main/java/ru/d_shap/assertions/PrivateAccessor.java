f69de59bcb6098289c19fa0d404bdaa1eb37be20
Dmitry Shapovalov <shenf@mail.ru>
2019-04-23 11:53:23 +0300

    Добавил методы для получения закрытых элементов объекта

--- src/main/java/ru/d_shap/assertions/PrivateAccessor.java
+++ src/main/java/ru/d_shap/assertions/PrivateAccessor.java
@@ -20,6 +20,9 @@
 package ru.d_shap.assertions;
 
 import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
@@ -32,6 +35,80 @@ public final class PrivateAccessor {
 
     private PrivateAccessor() {
         super();
+    }
+
+    /**
+     * Get the specified field.
+     *
+     * @param clazz     the class.
+     * @param object    the object or null for static field.
+     * @param fieldName the field name.
+     * @param <T>       the generic type of the class.
+     *
+     * @return the specified field.
+     *
+     * @throws NoSuchFieldException if the specified field can not be found.
+     */
+    public static <T> Field getField(final Class<T> clazz, final T object, final String fieldName) throws NoSuchFieldException {
+        Class<?> currentClazz = clazz;
+        NoSuchFieldException noSuchFieldException = null;
+        while (currentClazz != null) {
+            try {
+                Field field = currentClazz.getDeclaredField(fieldName);
+                setAccessible(field);
+                return field;
+            } catch (NoSuchFieldException ex) {
+                noSuchFieldException = ex;
+                currentClazz = currentClazz.getSuperclass();
+            }
+        }
+        throw noSuchFieldException;
+    }
+
+    /**
+     * Get the specified method.
+     *
+     * @param clazz          the class.
+     * @param object         the object or null for static method.
+     * @param methodName     the method name.
+     * @param parameterTypes the method parameter types.
+     * @param <T>            the generic type of the class.
+     *
+     * @return the specified method.
+     *
+     * @throws NoSuchMethodException if the specified method can not be found.
+     */
+    public static <T> Method getMethod(final Class<T> clazz, final T object, final String methodName, final Class<?>... parameterTypes) throws NoSuchMethodException {
+        Class<?> currentClazz = clazz;
+        NoSuchMethodException noSuchMethodException = null;
+        while (currentClazz != null) {
+            try {
+                Method method = currentClazz.getDeclaredMethod(methodName, parameterTypes);
+                setAccessible(method);
+                return method;
+            } catch (NoSuchMethodException ex) {
+                noSuchMethodException = ex;
+                currentClazz = currentClazz.getSuperclass();
+            }
+        }
+        throw noSuchMethodException;
+    }
+
+    /**
+     * Get the specified constructor.
+     *
+     * @param clazz          the class.
+     * @param parameterTypes the constructor parameter types.
+     * @param <T>            the generic type of the class.
+     *
+     * @return the specified constructor.
+     *
+     * @throws NoSuchMethodException if the specified constructor can not be found.
+     */
+    public static <T> Constructor<T> getConstructor(final Class<T> clazz, final Class<?>... parameterTypes) throws NoSuchMethodException {
+        Constructor<T> constructor = clazz.getDeclaredConstructor(parameterTypes);
+        setAccessible(constructor);
+        return constructor;
     }
 
     /**
