64213660d4268ed5226afcfa9608ee3106e3a1d8
Dmitry Shapovalov <shenf@mail.ru>
2019-04-29 21:03:41 +0300

    В проверке атрибута указывается название атрибута

--- src/main/java/ru/d_shap/assertions/Messages.java
+++ src/main/java/ru/d_shap/assertions/Messages.java
@@ -117,7 +117,7 @@ public final class Messages {
 
         public static final String QUALIFIED_NAME = "Check actual value''s qualified name.";
 
-        public static final String ATTRIBUTE = "Check actual value''s attribute.";
+        public static final String ATTRIBUTE = "Check actual value''s attribute: {0}.";
 
         public static final String DATA = "Check actual value''s data.";
 
