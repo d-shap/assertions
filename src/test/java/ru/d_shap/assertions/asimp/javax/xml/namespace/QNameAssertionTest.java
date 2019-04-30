5251377e8019f8d8169648f1ba3156ca3c901f60
Dmitry Shapovalov <shenf@mail.ru>
2019-04-30 12:52:05 +0300

    Тесты для qname

---
+++ src/test/java/ru/d_shap/assertions/asimp/javax/xml/namespace/QNameAssertionTest.java
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.javax.xml.namespace;
+
+import javax.xml.namespace.QName;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.assertions.Raw;
+
+/**
+ * Tests for {@link QNameAssertion}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class QNameAssertionTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public QNameAssertionTest() {
+        super();
+    }
+
+    /**
+     * {@link QNameAssertion} class test.
+     */
+    @Test
+    public void actualValueValidatorTest() {
+        initialize(Raw.qnameAssertion(), new QName("value"));
+
+        try {
+            initializeWithRawActual(Raw.qnameAssertion(), new Object());
+            Assertions.fail("QNameAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Actual value should match the assertion.\n\tActual value should be the instance of the expected class.\n\tExpected:<javax.xml.namespace.QName> but was:<java.lang.Object>");
+        }
+        try {
+            initializeWithRawActual(Raw.qnameAssertion(), new Object(), "Message");
+            Assertions.fail("QNameAssertion test fail");
+        } catch (AssertionError ex) {
+            Assertions.assertThat(ex).hasMessage("Message.\n\tActual value should match the assertion.\n\tActual value should be the instance of the expected class.\n\tExpected:<javax.xml.namespace.QName> but was:<java.lang.Object>");
+        }
+    }
+
+}
