d442ccdef001362f876b3c503f50836d8990b4d7
Dmitry Shapovalov <shenf@mail.ru>
2019-04-30 17:59:53 +0300

    Добавил заглушки для тестов

---
+++ src/test/java/ru/d_shap/assertions/asimp/org/w3c/dom/CharacterDataAssertionTest.java
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.org.w3c.dom;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+
+/**
+ * Tests for {@link CharacterDataAssertion}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class CharacterDataAssertionTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public CharacterDataAssertionTest() {
+        super();
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void actualValueValidatorTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void isEqualToTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void isNotEqualToTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void toDataTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void toDataMatcherTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void hasDataTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void isNullTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void isSameAsTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CharacterDataAssertion} class test.
+     */
+    @Test
+    public void isNotSameAsTest() {
+        // TODO
+    }
+
+}
