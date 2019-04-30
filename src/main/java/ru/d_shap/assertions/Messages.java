e211aa7eb82c330a52ec6c5a2813b2c7ad9f7202
Dmitry Shapovalov <shenf@mail.ru>
2019-04-30 12:21:50 +0300

    Добавил сообщение проверки

--- src/main/java/ru/d_shap/assertions/Messages.java
+++ src/main/java/ru/d_shap/assertions/Messages.java
@@ -113,6 +113,8 @@ public final class Messages {
 
         public static final String PREFIX = "Check actual value''s prefix.";
 
+        public static final String LOCAL_PART = "Check actual value''s local part.";
+
         public static final String LOCAL_NAME = "Check actual value''s local name.";
 
         public static final String QUALIFIED_NAME = "Check actual value''s qualified name.";
