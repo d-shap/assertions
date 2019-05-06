e7a2c9015456c07a8748ac72415b5f9772c889d1
Dmitry Shapovalov <shenf@mail.ru>
2019-05-06 14:37:35 +0300

    Добавил конвертер узла в строку

---
+++ src/main/java/ru/d_shap/assertions/asimp/org/w3c/dom/NodeAsStringConverter.java
@@ -0,0 +1,78 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.org.w3c.dom;
+
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Node;
+
+import ru.d_shap.assertions.converter.AsStringConverterProvider;
+import ru.d_shap.assertions.converter.ConversionException;
+import ru.d_shap.assertions.converter.ConverterArgumentHelper;
+
+/**
+ * Value to string converter for the node.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class NodeAsStringConverter implements AsStringConverterProvider {
+
+    private static final TransformerFactory TRANSFORMER_FACTORY = TransformerFactory.newInstance();
+
+    /**
+     * Create new object.
+     */
+    public NodeAsStringConverter() {
+        super();
+    }
+
+    @Override
+    public Class<?> getValueClass() {
+        return Node.class;
+    }
+
+    @Override
+    public String asString(final Object value) throws ConversionException {
+        Node castedValue = ConverterArgumentHelper.getValue(value, Node.class);
+
+        try {
+            Transformer transformer = TRANSFORMER_FACTORY.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
+            Source source = new DOMSource(castedValue);
+            StringWriter writer = new StringWriter();
+            Result result = new StreamResult(writer);
+            transformer.transform(source, result);
+            return writer.toString();
+        } catch (TransformerException ex) {
+            throw new ConversionException(ex);
+        }
+    }
+
+}
