32cccdbb688deceb151899a7a3ada2d267dbc585
Dmitry Shapovalov <shenf@mail.ru>
2019-05-17 11:25:33 +0300

    Поправил javadoc

--- src/main/java/ru/d_shap/assertions/fail/package-info.java
+++ src/main/java/ru/d_shap/assertions/fail/package-info.java
@@ -24,7 +24,23 @@
  * </p>
  * <p>
  * The fail description consists of the fail description entries, describing the check.
- * The last fail description entry can contain actual and expected value description.
+ * The last fail description entry can contain actual and expected value with an optional delta value.
+ * </p>
+ * <p>
+ * If the fail description contains the actual value, then this value is converted to the string
+ * representation.
+ * </p>
+ * <p>
+ * If the fail description contains the expected value, then the expected value is first converted
+ * to the value with the actual value's class, then the converted value is converted to the string
+ * representation. Conversion to the actual value's class can be skipped if the raw expected values
+ * are used.
+ * </p>
+ * <p>
+ * If the fail description contains the delta value, then the delta value is first converted to
+ * the value with the actual value's class, then the converted value is converted to the string
+ * representation. Conversion to the actual value's class can be replaced with the conversion to
+ * the expected value's class, if raw expected values are used.
  * </p>
  */
 package ru.d_shap.assertions.fail;
