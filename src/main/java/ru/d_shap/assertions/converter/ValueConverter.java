2f4bcf986b02cb678ea0e64ed50d1b7cc44d0d0b
Dmitry Shapovalov <shenf@mail.ru>
2022-01-12 10:46:46 +0300

    Убрал механизм SPI, сделал явный список конвертеров

--- src/main/java/ru/d_shap/assertions/converter/ValueConverter.java
+++ src/main/java/ru/d_shap/assertions/converter/ValueConverter.java
@@ -20,11 +20,47 @@
 package ru.d_shap.assertions.converter;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.ServiceLoader;
+
+import ru.d_shap.assertions.asimp.array.BooleanArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.BooleanArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.ByteArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.ByteArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.CharArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.CharArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.DoubleArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.DoubleArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.FloatArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.FloatArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.IntArrayToByteArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.IntArrayToCharArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.IntArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.IntArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.IntArrayToShortArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.LongArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.LongArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.array.ObjectArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.ShortArrayToListValueConverter;
+import ru.d_shap.assertions.asimp.array.ShortArrayToObjectArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.io.BufferedReaderToStringArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.io.InputStreamToByteArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.io.ReaderToCharArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.lang.CharSequenceToDocumentValueConverter;
+import ru.d_shap.assertions.asimp.java.lang.CharSequenceToElementValueConverter;
+import ru.d_shap.assertions.asimp.java.lang.IterableToListValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.ByteBufferToByteArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.CharBufferToCharArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.DoubleBufferToDoubleArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.FloatBufferToFloatArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.IntBufferToIntArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.LongBufferToLongArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.nio.ShortBufferToShortArrayValueConverter;
+import ru.d_shap.assertions.asimp.java.util.IteratorToListValueConverter;
+import ru.d_shap.assertions.asimp.primitive.IntToByteValueConverter;
+import ru.d_shap.assertions.asimp.primitive.IntToCharValueConverter;
+import ru.d_shap.assertions.asimp.primitive.IntToShortValueConverter;
 
 /**
  * Class to convert the value to the value with the target class.
@@ -37,12 +73,49 @@ public final class ValueConverter {
 
     static {
         CONVERTER_PROVIDERS = new LinkedList<>();
-        ServiceLoader<ValueConverterProvider> serviceLoader = ServiceLoader.load(ValueConverterProvider.class);
-        Iterator<ValueConverterProvider> iterator = serviceLoader.iterator();
-        while (iterator.hasNext()) {
-            ValueConverterProvider converterProvider = iterator.next();
-            CONVERTER_PROVIDERS.add(converterProvider);
-        }
+
+        CONVERTER_PROVIDERS.add(new IntToByteValueConverter());
+        CONVERTER_PROVIDERS.add(new IntToCharValueConverter());
+        CONVERTER_PROVIDERS.add(new IntToShortValueConverter());
+
+        CONVERTER_PROVIDERS.add(new CharSequenceToDocumentValueConverter());
+        CONVERTER_PROVIDERS.add(new CharSequenceToElementValueConverter());
+        CONVERTER_PROVIDERS.add(new IterableToListValueConverter());
+
+        CONVERTER_PROVIDERS.add(new IteratorToListValueConverter());
+
+        CONVERTER_PROVIDERS.add(new BooleanArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new BooleanArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new ByteArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new ByteArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new CharArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new CharArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new DoubleArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new DoubleArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new FloatArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new FloatArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new IntArrayToByteArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new IntArrayToCharArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new IntArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new IntArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new IntArrayToShortArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new LongArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new LongArrayToObjectArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new ObjectArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new ShortArrayToListValueConverter());
+        CONVERTER_PROVIDERS.add(new ShortArrayToObjectArrayValueConverter());
+
+        CONVERTER_PROVIDERS.add(new BufferedReaderToStringArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new InputStreamToByteArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new ReaderToCharArrayValueConverter());
+
+        CONVERTER_PROVIDERS.add(new ByteBufferToByteArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new CharBufferToCharArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new DoubleBufferToDoubleArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new FloatBufferToFloatArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new IntBufferToIntArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new LongBufferToLongArrayValueConverter());
+        CONVERTER_PROVIDERS.add(new ShortBufferToShortArrayValueConverter());
     }
 
     private static final Map<ConverterKey, ValueConverterProvider> CONVERTER_MAP = new HashMap<>();
