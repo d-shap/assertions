09d001e93dfa17384e55ec10d90bb2bd0db01159
Dmitry Shapovalov <shenf@mail.ru>
2022-01-24 09:26:25 +0300

    Конвертер даты в строку

---
+++ src/main/java/ru/d_shap/assertions/asimp/java/util/DateAsStringConverter.java
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.java.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import ru.d_shap.assertions.converter.AsStringConverterProvider;
+import ru.d_shap.assertions.converter.ConversionException;
+import ru.d_shap.assertions.converter.ConverterArgumentHelper;
+
+/**
+ * Value to string converter for the date.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class DateAsStringConverter implements AsStringConverterProvider {
+
+    /**
+     * Create new object.
+     */
+    public DateAsStringConverter() {
+        super();
+    }
+
+    @Override
+    public Class<?> getValueClass() {
+        return Date.class;
+    }
+
+    @Override
+    public String asString(final Object value) throws ConversionException {
+        Date castedValue = ConverterArgumentHelper.getValue(value, Date.class);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        return dateFormat.format(castedValue);
+    }
+
+}
