8f0d096b69e5637f402f1bf26cc5f117533dec1d
Dmitry Shapovalov <shenf@mail.ru>
2022-02-12 17:08:51 +0300

    Конвертер из описания в строку

---
+++ src/main/java/ru/d_shap/assertions/asimp/ExecutableDescriptionAsStringConverter.java
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp;
+
+import ru.d_shap.assertions.converter.AsStringConverter;
+import ru.d_shap.assertions.converter.AsStringConverterProvider;
+import ru.d_shap.assertions.converter.ConverterArgumentHelper;
+
+/**
+ * Value to string converter for the executable description.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ExecutableDescriptionAsStringConverter implements AsStringConverterProvider {
+
+    /**
+     * Create new object.
+     */
+    public ExecutableDescriptionAsStringConverter() {
+        super();
+    }
+
+    @Override
+    public Class<?> getValueClass() {
+        return ExecutableDescription.class;
+    }
+
+    @Override
+    public String asString(final Object value) {
+        ExecutableDescription castedValue = ConverterArgumentHelper.getValue(value, ExecutableDescription.class);
+
+        StringBuilder result = new StringBuilder();
+        result.append(castedValue.getName());
+        result.append('(');
+        boolean first = true;
+        for (Class<?> parameterType : castedValue.getParameterTypes()) {
+            if (first) {
+                first = false;
+            } else {
+                result.append(", ");
+            }
+            result.append(AsStringConverter.asString(parameterType));
+        }
+        result.append(')');
+        return result.toString();
+    }
+
+}
