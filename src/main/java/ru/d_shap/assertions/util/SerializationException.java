cd629a32c16ca208de0b2d5cd420ef1b6e69327c
Dmitry Shapovalov <shenf@mail.ru>
2022-02-17 23:04:04 +0300

    Ошибка сериализации

---
+++ src/main/java/ru/d_shap/assertions/util/SerializationException.java
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.util;
+
+/**
+ * Wrapper for exceptions, that can occur during serialization and deserialization.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class SerializationException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create new object.
+     *
+     * @param cause the cause of the exception.
+     */
+    public SerializationException(final Throwable cause) {
+        super(getMessage(cause), cause);
+    }
+
+    private static String getMessage(final Throwable cause) {
+        if (cause == null) {
+            return null;
+        } else {
+            return cause.getMessage();
+        }
+    }
+
+}
