822e88bdfbd033f56ff355267081356ed640ec97
Dmitry Shapovalov <shenf@mail.ru>
2022-03-25 18:01:19 +0300

    Поправил проверки с учетом аргумента конвертации

--- src/test/java/ru/d_shap/assertions/asimp/primitive/IntToStringValueConverterTest.java
+++ src/test/java/ru/d_shap/assertions/asimp/primitive/IntToStringValueConverterTest.java
@@ -60,53 +60,77 @@ public final class IntToStringValueConverterTest extends AssertionTest {
      */
     @Test
     public void convertTest() {
-        Assertions.assertThat(new IntToStringValueConverter().convert(0)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(0), Raw.charSequenceAssertion()).isEqualTo("00000000");
-        Assertions.assertThat(new IntToStringValueConverter().convert(0)).as(Raw.charSequenceAssertion()).isEqualTo("00000000");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, (Boolean) null)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, (Boolean) null), Raw.charSequenceAssertion()).isEqualTo("0");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, (Boolean) null)).as(Raw.charSequenceAssertion()).isEqualTo("0");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(1)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(1), Raw.charSequenceAssertion()).isEqualTo("00000001");
-        Assertions.assertThat(new IntToStringValueConverter().convert(1)).as(Raw.charSequenceAssertion()).isEqualTo("00000001");
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, (Boolean) null)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, (Boolean) null), Raw.charSequenceAssertion()).isEqualTo("100");
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, (Boolean) null)).as(Raw.charSequenceAssertion()).isEqualTo("100");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(31)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(31), Raw.charSequenceAssertion()).isEqualTo("0000001f");
-        Assertions.assertThat(new IntToStringValueConverter().convert(31)).as(Raw.charSequenceAssertion()).isEqualTo("0000001f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, (Boolean) null)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, (Boolean) null), Raw.charSequenceAssertion()).isEqualTo("2147483647");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, (Boolean) null)).as(Raw.charSequenceAssertion()).isEqualTo("2147483647");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(479)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(479), Raw.charSequenceAssertion()).isEqualTo("000001df");
-        Assertions.assertThat(new IntToStringValueConverter().convert(479)).as(Raw.charSequenceAssertion()).isEqualTo("000001df");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, false)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, false), Raw.charSequenceAssertion()).isEqualTo("0");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, false)).as(Raw.charSequenceAssertion()).isEqualTo("0");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(7311)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(7311), Raw.charSequenceAssertion()).isEqualTo("00001c8f");
-        Assertions.assertThat(new IntToStringValueConverter().convert(7311)).as(Raw.charSequenceAssertion()).isEqualTo("00001c8f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, false)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, false), Raw.charSequenceAssertion()).isEqualTo("100");
+        Assertions.assertThat(new IntToStringValueConverter().convert(100, false)).as(Raw.charSequenceAssertion()).isEqualTo("100");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(102351)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(102351), Raw.charSequenceAssertion()).isEqualTo("00018fcf");
-        Assertions.assertThat(new IntToStringValueConverter().convert(102351)).as(Raw.charSequenceAssertion()).isEqualTo("00018fcf");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, false)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, false), Raw.charSequenceAssertion()).isEqualTo("2147483647");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, false)).as(Raw.charSequenceAssertion()).isEqualTo("2147483647");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(1300879)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(1300879), Raw.charSequenceAssertion()).isEqualTo("0013d98f");
-        Assertions.assertThat(new IntToStringValueConverter().convert(1300879)).as(Raw.charSequenceAssertion()).isEqualTo("0013d98f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, true), Raw.charSequenceAssertion()).isEqualTo("00000000");
+        Assertions.assertThat(new IntToStringValueConverter().convert(0, true)).as(Raw.charSequenceAssertion()).isEqualTo("00000000");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(26461823)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(26461823), Raw.charSequenceAssertion()).isEqualTo("0193c67f");
-        Assertions.assertThat(new IntToStringValueConverter().convert(26461823)).as(Raw.charSequenceAssertion()).isEqualTo("0193c67f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(1, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(1, true), Raw.charSequenceAssertion()).isEqualTo("00000001");
+        Assertions.assertThat(new IntToStringValueConverter().convert(1, true)).as(Raw.charSequenceAssertion()).isEqualTo("00000001");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE), Raw.charSequenceAssertion()).isEqualTo("7fffffff");
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE)).as(Raw.charSequenceAssertion()).isEqualTo("7fffffff");
+        Assertions.assertThat(new IntToStringValueConverter().convert(31, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(31, true), Raw.charSequenceAssertion()).isEqualTo("0000001f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(31, true)).as(Raw.charSequenceAssertion()).isEqualTo("0000001f");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L), Raw.charSequenceAssertion()).isEqualTo("ffffffff");
-        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L)).as(Raw.charSequenceAssertion()).isEqualTo("ffffffff");
+        Assertions.assertThat(new IntToStringValueConverter().convert(479, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(479, true), Raw.charSequenceAssertion()).isEqualTo("000001df");
+        Assertions.assertThat(new IntToStringValueConverter().convert(479, true)).as(Raw.charSequenceAssertion()).isEqualTo("000001df");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(-100)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(-100), Raw.charSequenceAssertion()).isEqualTo("ffffff9c");
-        Assertions.assertThat(new IntToStringValueConverter().convert(-100)).as(Raw.charSequenceAssertion()).isEqualTo("ffffff9c");
+        Assertions.assertThat(new IntToStringValueConverter().convert(7311, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(7311, true), Raw.charSequenceAssertion()).isEqualTo("00001c8f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(7311, true)).as(Raw.charSequenceAssertion()).isEqualTo("00001c8f");
 
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE)).isInstanceOf(String.class);
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE), Raw.charSequenceAssertion()).isEqualTo("80000000");
-        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE)).as(Raw.charSequenceAssertion()).isEqualTo("80000000");
+        Assertions.assertThat(new IntToStringValueConverter().convert(102351, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(102351, true), Raw.charSequenceAssertion()).isEqualTo("00018fcf");
+        Assertions.assertThat(new IntToStringValueConverter().convert(102351, true)).as(Raw.charSequenceAssertion()).isEqualTo("00018fcf");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert(1300879, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(1300879, true), Raw.charSequenceAssertion()).isEqualTo("0013d98f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(1300879, true)).as(Raw.charSequenceAssertion()).isEqualTo("0013d98f");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert(26461823, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(26461823, true), Raw.charSequenceAssertion()).isEqualTo("0193c67f");
+        Assertions.assertThat(new IntToStringValueConverter().convert(26461823, true)).as(Raw.charSequenceAssertion()).isEqualTo("0193c67f");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, true), Raw.charSequenceAssertion()).isEqualTo("7fffffff");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MAX_VALUE, true)).as(Raw.charSequenceAssertion()).isEqualTo("7fffffff");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L, true), Raw.charSequenceAssertion()).isEqualTo("ffffffff");
+        Assertions.assertThat(new IntToStringValueConverter().convert((int) 4294967295L, true)).as(Raw.charSequenceAssertion()).isEqualTo("ffffffff");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert(-100, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(-100, true), Raw.charSequenceAssertion()).isEqualTo("ffffff9c");
+        Assertions.assertThat(new IntToStringValueConverter().convert(-100, true)).as(Raw.charSequenceAssertion()).isEqualTo("ffffff9c");
+
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE, true)).isInstanceOf(String.class);
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE, true), Raw.charSequenceAssertion()).isEqualTo("80000000");
+        Assertions.assertThat(new IntToStringValueConverter().convert(Integer.MIN_VALUE, true)).as(Raw.charSequenceAssertion()).isEqualTo("80000000");
     }
 
     /**
@@ -129,8 +153,16 @@ public final class IntToStringValueConverterTest extends AssertionTest {
      * {@link IntToStringValueConverter} class test.
      */
     @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void convertWrongArgumentCountFailTest() {
-        new IntToStringValueConverter().convert(0, new Object());
+    public void convertWrongArgumentCount0FailTest() {
+        new IntToStringValueConverter().convert(0);
+    }
+
+    /**
+     * {@link IntToStringValueConverter} class test.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void convertWrongArgumentCount2FailTest() {
+        new IntToStringValueConverter().convert(0, new Object(), new Object());
     }
 
 }
