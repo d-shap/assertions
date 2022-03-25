595cd28d30e9f4debfe805f2f84d7e7a341c0929
Dmitry Shapovalov <shenf@mail.ru>
2022-03-25 15:37:53 +0300

    Конвертер в шестнадцатеричные значения байтов

---
+++ src/main/java/ru/d_shap/assertions/asimp/primitive/ByteToStringValueConverter.java
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.primitive;
+
+import ru.d_shap.assertions.converter.ConverterArgumentHelper;
+import ru.d_shap.assertions.converter.ValueConverterProvider;
+
+/**
+ * Value converter from the byte to the string.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ByteToStringValueConverter implements ValueConverterProvider {
+
+    private static final String[] VALUES;
+
+    static {
+        VALUES = new String[16];
+        VALUES[0] = "0";
+        VALUES[1] = "1";
+        VALUES[2] = "2";
+        VALUES[3] = "3";
+        VALUES[4] = "4";
+        VALUES[5] = "5";
+        VALUES[6] = "6";
+        VALUES[7] = "7";
+        VALUES[8] = "8";
+        VALUES[9] = "9";
+        VALUES[10] = "a";
+        VALUES[11] = "b";
+        VALUES[12] = "c";
+        VALUES[13] = "d";
+        VALUES[14] = "e";
+        VALUES[15] = "f";
+    }
+
+    /**
+     * Create new object.
+     */
+    public ByteToStringValueConverter() {
+        super();
+    }
+
+    @Override
+    public Class<?> getValueClass() {
+        return Byte.class;
+    }
+
+    @Override
+    public Class<?> getTargetClass() {
+        return String.class;
+    }
+
+    @Override
+    public Object convert(final Object value, final Object... arguments) {
+        byte castedValue = ConverterArgumentHelper.getValue(value, Byte.class);
+        ConverterArgumentHelper.checkArgumentsLength(arguments, 0);
+
+        int idx1 = castedValue >> 4 & 0xF;
+        int idx2 = castedValue & 0xF;
+        return VALUES[idx1] + VALUES[idx2];
+    }
+
+}
