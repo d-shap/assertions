a5591b67332dae34a3408d808acc9438864ba212
Dmitry Shapovalov <shenf@mail.ru>
2022-03-29 23:59:25 +0300

    Заглушка для тестов

---
+++ src/test/java/ru/d_shap/assertions/asimp/HexStringTest.java
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+
+/**
+ * Tests for {@link HexString}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class HexStringTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public HexStringTest() {
+        super();
+    }
+
+    /**
+     * {@link HexString} class test.
+     */
+    @Test
+    public void addByteValueTest() {
+        // TODO
+    }
+
+    /**
+     * {@link HexString} class test.
+     */
+    @Test
+    public void addShortValueTest() {
+        // TODO
+    }
+
+    /**
+     * {@link HexString} class test.
+     */
+    @Test
+    public void addIntegerValueTest() {
+        // TODO
+    }
+
+    /**
+     * {@link HexString} class test.
+     */
+    @Test
+    public void addLongValueTest() {
+        // TODO
+    }
+
+}
