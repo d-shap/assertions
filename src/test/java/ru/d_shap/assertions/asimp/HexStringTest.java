f0dcfd916e1ce0f67778f8be311b176661c500a5
Dmitry Shapovalov <shenf@mail.ru>
2022-03-30 16:50:38 +0300

    добавил тесты

--- src/test/java/ru/d_shap/assertions/asimp/HexStringTest.java
+++ src/test/java/ru/d_shap/assertions/asimp/HexStringTest.java
@@ -22,6 +22,7 @@ package ru.d_shap.assertions.asimp;
 import org.junit.Test;
 
 import ru.d_shap.assertions.AssertionTest;
+import ru.d_shap.assertions.Assertions;
 
 /**
  * Tests for {@link HexString}.
@@ -42,7 +43,41 @@ public final class HexStringTest extends AssertionTest {
      */
     @Test
     public void addByteValueTest() {
-        // TODO
+        HexString hexString01 = new HexString();
+        hexString01.addValue((byte) 0);
+        Assertions.assertThat(hexString01).hasToString("00");
+
+        HexString hexString02 = new HexString();
+        hexString02.addValue((byte) 1);
+        Assertions.assertThat(hexString02).hasToString("01");
+
+        HexString hexString03 = new HexString();
+        hexString03.addValue((byte) 31);
+        Assertions.assertThat(hexString03).hasToString("1f");
+
+        HexString hexString04 = new HexString();
+        hexString04.addValue((byte) 100);
+        Assertions.assertThat(hexString04).hasToString("64");
+
+        HexString hexString05 = new HexString();
+        hexString05.addValue(Byte.MAX_VALUE);
+        Assertions.assertThat(hexString05).hasToString("7f");
+
+        HexString hexString06 = new HexString();
+        hexString06.addValue((byte) -1);
+        Assertions.assertThat(hexString06).hasToString("ff");
+
+        HexString hexString07 = new HexString();
+        hexString07.addValue((byte) -100);
+        Assertions.assertThat(hexString07).hasToString("9c");
+
+        HexString hexString08 = new HexString();
+        hexString08.addValue(Byte.MIN_VALUE);
+        Assertions.assertThat(hexString08).hasToString("80");
+        hexString08.addValue((byte) 44);
+        Assertions.assertThat(hexString08).hasToString("802c");
+        hexString08.addValue((byte) -97);
+        Assertions.assertThat(hexString08).hasToString("802c9f");
     }
 
     /**
@@ -50,7 +85,45 @@ public final class HexStringTest extends AssertionTest {
      */
     @Test
     public void addShortValueTest() {
-        // TODO
+        HexString hexString01 = new HexString();
+        hexString01.addValue((short) 0);
+        Assertions.assertThat(hexString01).hasToString("0000");
+
+        HexString hexString02 = new HexString();
+        hexString02.addValue((short) 1);
+        Assertions.assertThat(hexString02).hasToString("0001");
+
+        HexString hexString03 = new HexString();
+        hexString03.addValue((short) 31);
+        Assertions.assertThat(hexString03).hasToString("001f");
+
+        HexString hexString04 = new HexString();
+        hexString04.addValue((short) 479);
+        Assertions.assertThat(hexString04).hasToString("01df");
+
+        HexString hexString05 = new HexString();
+        hexString05.addValue((short) 7311);
+        Assertions.assertThat(hexString05).hasToString("1c8f");
+
+        HexString hexString06 = new HexString();
+        hexString06.addValue(Short.MAX_VALUE);
+        Assertions.assertThat(hexString06).hasToString("7fff");
+
+        HexString hexString07 = new HexString();
+        hexString07.addValue((short) -1);
+        Assertions.assertThat(hexString07).hasToString("ffff");
+
+        HexString hexString08 = new HexString();
+        hexString08.addValue((short) -100);
+        Assertions.assertThat(hexString08).hasToString("ff9c");
+
+        HexString hexString09 = new HexString();
+        hexString09.addValue(Short.MIN_VALUE);
+        Assertions.assertThat(hexString09).hasToString("8000");
+        hexString09.addValue((short) 10347);
+        Assertions.assertThat(hexString09).hasToString("8000286b");
+        hexString09.addValue((short) -22978);
+        Assertions.assertThat(hexString09).hasToString("8000286ba63e");
     }
 
     /**
