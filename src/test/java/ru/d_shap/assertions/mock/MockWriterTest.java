4cc0f818601e3c8f928aae8dfc1787f981e23bc1
Dmitry Shapovalov <shenf@mail.ru>
2022-05-09 16:11:08 +0300

    Добавил заглушки для тестов

---
+++ src/test/java/ru/d_shap/assertions/mock/MockWriterTest.java
@@ -0,0 +1,102 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.mock;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+
+/**
+ * Tests for {@link MockWriter}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class MockWriterTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public MockWriterTest() {
+        super();
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void writeCharTest() throws IOException {
+        // TODO
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void writeCharArrayTest() throws IOException {
+        // TODO
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void flushTest() throws IOException {
+        // TODO
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void closeTest() throws IOException {
+        // TODO
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void getContentTest() throws IOException {
+        // TODO
+    }
+
+    /**
+     * {@link MockWriter} class test.
+     *
+     * @throws IOException exception in test.
+     */
+    @Test
+    public void isClosedTest() throws IOException {
+        // TODO
+    }
+
+}
