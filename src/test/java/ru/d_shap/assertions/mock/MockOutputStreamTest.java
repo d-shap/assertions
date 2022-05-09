62d40e1480f7bc8edc8cef24e4234f7725b4a520
Dmitry Shapovalov <shenf@mail.ru>
2022-05-09 17:22:48 +0300

    Поправил проверки, убрал локальные переменные

--- src/test/java/ru/d_shap/assertions/mock/MockOutputStreamTest.java
+++ src/test/java/ru/d_shap/assertions/mock/MockOutputStreamTest.java
@@ -150,66 +150,92 @@ public final class MockOutputStreamTest extends AssertionTest {
      */
     @Test
     public void writeByteArrayTest() throws IOException {
-        OutputStream outputStream01 = DataHelper.createOutputStreamBuilder().setContentSize(100).buildOutputStream();
-        byte[] buff011 = new byte[]{11, 12, 13};
-        outputStream01.write(buff011, 0, 3);
+        OutputStream outputStream01 = DataHelper.createOutputStreamBuilder().setContentSize(10).buildOutputStream();
+        outputStream01.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream01).getContent()).containsExactlyInOrder(11, 12, 13);
-        byte[] buff012 = new byte[]{21, 22, 23};
-        outputStream01.write(buff012, 1, 2);
+        outputStream01.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream01).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23);
-        byte[] buff013 = new byte[]{31, 32, 33};
-        outputStream01.write(buff013, 0, 2);
+        outputStream01.write(new byte[]{31, 32, 33}, 0, 2);
         Assertions.assertThat(((MockOutputStream) outputStream01).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23, 31, 32);
-        byte[] buff014 = new byte[]{41, 0, 43};
-        outputStream01.write(buff014, 1, 1);
+        outputStream01.write(new byte[]{41, 0, 43}, 1, 1);
         Assertions.assertThat(((MockOutputStream) outputStream01).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23, 31, 32, 0);
 
         OutputStream outputStream02 = DataHelper.createOutputStreamBuilder().setContentSize(6).buildOutputStream();
-        byte[] buff021 = new byte[]{11, 12, 13};
-        outputStream02.write(buff021, 0, 3);
+        outputStream02.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream02).getContent()).containsExactlyInOrder(11, 12, 13);
-        byte[] buff022 = new byte[]{21, 22, 23};
-        outputStream02.write(buff022, 1, 2);
+        outputStream02.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream02).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23);
-        byte[] buff023 = new byte[]{31, 32, 33};
-        outputStream02.write(buff023, 0, 2);
+        outputStream02.write(new byte[]{31, 32, 33}, 0, 2);
         Assertions.assertThat(((MockOutputStream) outputStream02).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23, 31);
-        byte[] buff024 = new byte[]{41, 0, 43};
-        outputStream02.write(buff024, 1, 1);
+        outputStream02.write(new byte[]{41, 0, 43}, 1, 1);
         Assertions.assertThat(((MockOutputStream) outputStream02).getContent()).containsExactlyInOrder(11, 12, 13, 22, 23, 31);
 
         OutputStream outputStream03 = DataHelper.createOutputStreamBuilder().setContentSize(0).buildOutputStream();
-        byte[] buff031 = new byte[]{11, 12, 13};
-        outputStream03.write(buff031, 0, 3);
+        outputStream03.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream03).getContent()).containsExactlyInOrder();
-        byte[] buff032 = new byte[]{21, 22, 23};
-        outputStream03.write(buff032, 1, 2);
+        outputStream03.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream03).getContent()).containsExactlyInOrder();
 
         OutputStream outputStream04 = DataHelper.createOutputStreamBuilder().buildOutputStream();
-        byte[] buff041 = new byte[]{11, 12, 13};
-        outputStream04.write(buff041, 0, 3);
+        outputStream04.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream04).getContent()).containsExactlyInOrder();
-        byte[] buff042 = new byte[]{21, 22, 23};
-        outputStream04.write(buff042, 1, 2);
+        outputStream04.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream04).getContent()).containsExactlyInOrder();
 
         OutputStream outputStream05 = DataHelper.createOutputStreamBuilder().setContentSize(2).setFlushException("ex").buildOutputStream();
-        byte[] buff051 = new byte[]{11, 12, 13};
-        outputStream05.write(buff051, 0, 3);
+        outputStream05.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream05).getContent()).containsExactlyInOrder(11, 12);
-        byte[] buff052 = new byte[]{21, 22, 23};
-        outputStream05.write(buff052, 1, 2);
+        outputStream05.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream05).getContent()).containsExactlyInOrder(11, 12);
 
         OutputStream outputStream06 = DataHelper.createOutputStreamBuilder().setContentSize(2).setCloseException("ex").buildOutputStream();
-        byte[] buff061 = new byte[]{11, 12, 13};
-        outputStream06.write(buff061, 0, 3);
+        outputStream06.write(new byte[]{11, 12, 13}, 0, 3);
         Assertions.assertThat(((MockOutputStream) outputStream06).getContent()).containsExactlyInOrder(11, 12);
-        byte[] buff062 = new byte[]{21, 22, 23};
-        outputStream06.write(buff062, 1, 2);
+        outputStream06.write(new byte[]{21, 22, 23}, 1, 2);
         Assertions.assertThat(((MockOutputStream) outputStream06).getContent()).containsExactlyInOrder(11, 12);
 
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(null, 0, 10);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (NullPointerException ex) {
+            Assertions.assertThat(ex).hasMessage("buffer is null");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(null, -1, 10);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (NullPointerException ex) {
+            Assertions.assertThat(ex).hasMessage("buffer is null");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(new byte[]{1, 2, 3, 4, 5}, -1, 10);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (IndexOutOfBoundsException ex) {
+            Assertions.assertThat(ex).hasMessage("offset is out of bounds");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(new byte[]{1, 2, 3, 4, 5}, 5, 10);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (IndexOutOfBoundsException ex) {
+            Assertions.assertThat(ex).hasMessage("offset is out of bounds");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(new byte[]{1, 2, 3, 4, 5}, 0, -1);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (IndexOutOfBoundsException ex) {
+            Assertions.assertThat(ex).hasMessage("length is out of bounds");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(new byte[]{1, 2, 3, 4, 5}, 0, 6);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (IndexOutOfBoundsException ex) {
+            Assertions.assertThat(ex).hasMessage("length is out of bounds");
+        }
+        try {
+            DataHelper.createOutputStreamBuilder().buildOutputStream().write(new byte[]{1, 2, 3, 4, 5}, 4, 2);
+            Assertions.fail("MockOutputStream test fail");
+        } catch (IndexOutOfBoundsException ex) {
+            Assertions.assertThat(ex).hasMessage("length is out of bounds");
+        }
         try {
             DataHelper.createOutputStreamBuilder().setWriteException(new IOException("fail")).buildOutputStream().write(new byte[]{1}, 0, 1);
             Assertions.fail("MockOutputStream test fail");
