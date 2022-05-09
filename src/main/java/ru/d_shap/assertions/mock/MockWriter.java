748c7cead536d26d222c32e1d7205f6194a0fb83
Dmitry Shapovalov <shenf@mail.ru>
2022-05-09 16:01:10 +0300

    Изменил тип возвращаемого результата

--- src/main/java/ru/d_shap/assertions/mock/MockWriter.java
+++ src/main/java/ru/d_shap/assertions/mock/MockWriter.java
@@ -247,7 +247,7 @@ public final class MockWriter extends Writer implements IsCloseable {
          *
          * @return the created mock object.
          */
-        public MockWriter buildWriter() {
+        public Writer buildWriter() {
             return new MockWriter(_contentSize, _writeException, _flushException, _closeException);
         }
 
