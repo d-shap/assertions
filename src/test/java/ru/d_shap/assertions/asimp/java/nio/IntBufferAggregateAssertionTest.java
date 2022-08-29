d5b1596f17950f2995013f94f16dbc5fb06f281a
Dmitry Shapovalov <shenf@mail.ru>
2022-08-29 12:00:17 +0300

    Сделал заглушки для тестов

---
+++ src/test/java/ru/d_shap/assertions/asimp/java/nio/IntBufferAggregateAssertionTest.java
@@ -0,0 +1,168 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.java.nio;
+
+import org.junit.Test;
+
+import ru.d_shap.assertions.AssertionTest;
+
+/**
+ * Tests for {@link IntBufferAssertion}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class IntBufferAggregateAssertionTest extends AssertionTest {
+
+    /**
+     * Test class constructor.
+     */
+    public IntBufferAggregateAssertionTest() {
+        super();
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toMinTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toMinMatcherTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toRewindAndMinTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toRewindAndMinMatcherTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasMinPrimitiveTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasMinObjectTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasRewindAndMinPrimitiveTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasRewindAndMinObjectTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toMaxTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toMaxMatcherTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toRewindAndMaxTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void toRewindAndMaxMatcherTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasMaxPrimitiveTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasMaxObjectTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasRewindAndMaxPrimitiveTest() {
+        // TODO
+    }
+
+    /**
+     * {@link IntBufferAssertion} class test.
+     */
+    @Test
+    public void hasRewindAndMaxObjectTest() {
+        // TODO
+    }
+
+}
