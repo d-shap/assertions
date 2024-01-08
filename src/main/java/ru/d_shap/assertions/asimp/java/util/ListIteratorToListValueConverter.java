51f007dffc40c960edf27d3db58a7ccda2dbcd81
Dmitry Shapovalov <shenf@mail.ru>
2024-01-08 13:25:39 +0300

    Конвертер из list iterator в list

---
+++ src/main/java/ru/d_shap/assertions/asimp/java/util/ListIteratorToListValueConverter.java
@@ -0,0 +1,75 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Assertions library provides facilities for the unit testing.
+// Copyright (C) 2017 Dmitry Shapovalov.
+//
+// This file is part of assertions library.
+//
+// Assertions library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Assertions library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.assertions.asimp.java.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import ru.d_shap.assertions.converter.ConverterArgumentHelper;
+import ru.d_shap.assertions.converter.ValueConverterProvider;
+
+/**
+ * Value converter from the list iterator to the list.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ListIteratorToListValueConverter implements ValueConverterProvider {
+
+    /**
+     * Create new object.
+     */
+    public ListIteratorToListValueConverter() {
+        super();
+    }
+
+    @Override
+    public Class<?> getValueClass() {
+        return ListIterator.class;
+    }
+
+    @Override
+    public Class<?> getTargetClass() {
+        return List.class;
+    }
+
+    @Override
+    public Object convert(final Object value, final Object... arguments) {
+        ListIterator<?> castedValue = ConverterArgumentHelper.getValue(value, ListIterator.class);
+        ConverterArgumentHelper.checkArgumentsLength(arguments, 2);
+        int castedCount = ConverterArgumentHelper.getArgument(arguments, 0, Integer.class);
+        boolean castedForward = ConverterArgumentHelper.getArgument(arguments, 1, Boolean.class);
+
+        List<Object> result = new ArrayList<>();
+        if (castedForward) {
+            for (int i = 0; (castedCount == 0 || i < castedCount) && castedValue.hasNext(); i++) {
+                Object element = castedValue.next();
+                result.add(element);
+            }
+        } else {
+            for (int i = 0; (castedCount == 0 || i < castedCount) && castedValue.hasPrevious(); i++) {
+                Object element = castedValue.previous();
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+}
